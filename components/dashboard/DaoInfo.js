018032c576bcd08ad6beb2f3cec4fd5cc4e37fa5
nerderlyne <shivanshityagi@icloud.com>
2022-01-19 03:59:26 +0530

    dao info external link

--- components/dashboard/DaoInfo.js
+++ components/dashboard/DaoInfo.js
@@ -1,6 +1,15 @@
 import { useState, useContext, useEffect } from "react";
 import AppContext from "../../context/AppContext";
-import { Text, HStack, Link, Icon, Button, Divider, Spacer, Center } from "@chakra-ui/react";
+import {
+  Text,
+  HStack,
+  Link,
+  Icon,
+  Button,
+  Divider,
+  Spacer,
+  Center,
+} from "@chakra-ui/react";
 import {
   Modal,
   ModalOverlay,
@@ -9,7 +18,7 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-} from '@chakra-ui/react'
+} from "@chakra-ui/react";
 import { BsFillArrowUpRightSquareFill } from "react-icons/bs";
 import { useDisclosure } from "@chakra-ui/react";
 import CapTable from "./CapTable";
@@ -19,84 +28,83 @@ import DashedDivider from "../elements/DashedDivider";
 export default function DaoInfo() {
   const value = useContext(AppContext);
   const { dao } = value.state;
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   const array = [
     {
       name: "Name",
-      info: dao['name'],
-      link: null
+      info: dao["name"],
+      link: null,
     },
     {
       name: "Address",
-      info: dao['address'],
-      link: `https://rinkeby.etherscan.io/address/${dao["address"]}`
+      info: dao["address"],
+      link: `https://rinkeby.etherscan.io/address/${dao["address"]}`,
     },
     {
       name: "Symbol",
-      info: dao['token']['symbol'],
-      link: null
+      info: dao["token"]["symbol"],
+      link: null,
     },
     {
       name: "Shares",
-      info: fromDecimals(dao['token']['totalSupply'], 18),
-      link: null
+      info: fromDecimals(dao["token"]["totalSupply"], 18),
+      link: null,
     },
     {
       name: "Docs",
-      info: dao['docs'],
-      link: `${dao["docs"]}`
+      info: dao["docs"],
+      link: `${dao["docs"]}`,
     },
     {
       name: "Members",
-      info: dao['members'].length,
-      link: null
-    }
-  ]
+      info: dao["members"].length,
+      link: null,
+    },
+  ];
 
-  return(
+  return (
     <div>
-    {array.map((item, index) => (
-      <>
-      {item.info != undefined ?
+      {array.map((item, index) => (
         <>
-      <HStack>
-        <Text>{item.name}</Text>
-        <Spacer />
-        <Text>{item.info}</Text>
-        {item.link != null ?
-        <Link
-          passHref
-          href={item.link}
-        >
-          <Icon as={BsFillArrowUpRightSquareFill} />
-        </Link>
-        : null}
-      </HStack>
-      <DashedDivider />
-      </>
-      : null}
-      </>
-    ))}
-    <Center>
-      <Button className="transparent-btn" onClick={onOpen}>View Cap Table</Button>
-    </Center>
-    <Modal isOpen={isOpen} onClose={onClose} size="xl">
-    <ModalOverlay />
-    <ModalContent>
-      <ModalHeader>Cap Table</ModalHeader>
-      <ModalCloseButton />
-      <ModalBody>
-        <CapTable />
-      </ModalBody>
-
-      <ModalFooter>
-        <Button colorScheme='blue' mr={3} onClick={onClose}>
-          Close
+          {item.info != undefined ? (
+            <>
+              <HStack>
+                <Text>{item.name}</Text>
+                <Spacer />
+                <Text>{item.info}</Text>
+                {item.link != null ? (
+                  <Link passHref isExternal href={item.link}>
+                    <Icon as={BsFillArrowUpRightSquareFill} />
+                  </Link>
+                ) : null}
+              </HStack>
+              <DashedDivider />
+            </>
+          ) : null}
+        </>
+      ))}
+      <Center>
+        <Button className="transparent-btn" onClick={onOpen}>
+          View Cap Table
         </Button>
-      </ModalFooter>
-    </ModalContent>
-  </Modal>
-  </div>
+      </Center>
+      <Modal isOpen={isOpen} onClose={onClose} size="xl">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Cap Table</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <CapTable />
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </div>
   );
 }
