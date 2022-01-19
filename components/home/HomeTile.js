6b8829ef19cc33ddeddff135b433b5a0c2289b1a
nerderlyne <shivanshityagi@icloud.com>
2022-01-19 15:03:27 +0530

    rm

--- components/home/HomeTile.js
+++ components/home/HomeTile.js
@@ -1,19 +1,18 @@
-import { Button, Box, Text, VStack, HStack, Image, Divider } from "@chakra-ui/react";
+import { Text, VStack, HStack } from "@chakra-ui/react";
 import ContactForm from "../elements/ContactForm";
-import FlexGradient from "../elements/FlexGradient";
 import KaliButton from "../elements/KaliButton";
 
 export default function HomeTile(props) {
   return (
-      <HStack spacing={250} p={250} alignItems="center">
-        <VStack>
-          <Text fontSize="xl">
-            <b>Simplify your Business with Smart Agreements</b>
-          </Text>
-          <br></br>
-          <KaliButton onClick={props.setDeployerVisible}>Get Started</KaliButton>
-          {/* <ContactForm /> */}
-        </VStack>
-      </HStack>
+    <HStack spacing={250} p={250} alignItems="center">
+      <VStack>
+        <Text fontSize="xl">
+          <b>Simplify your Business with Smart Agreements</b>
+        </Text>
+        <br></br>
+        <KaliButton onClick={props.setDeployerVisible}>Get Started</KaliButton>
+        {/* <ContactForm /> */}
+      </VStack>
+    </HStack>
   );
 }
