796c257cb1b8b2a5c0ef08108c78b410b08b7990
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-01-26 16:30:52 -0500

    Update Footer.js
    💄add link to ToS and discord button

--- components/structure/Footer.js
+++ components/structure/Footer.js
@@ -6,7 +6,7 @@ import {
   Spacer,
   useColorModeValue,
 } from "@chakra-ui/react";
-import { FiTwitter, FiGithub } from "react-icons/fi";
+import { FiTwitter, FiGithub, FiUsers } from "react-icons/fi";
 
 const SocialButton = (props) => {
   return (
@@ -33,13 +33,21 @@ export default function Footer() {
           <i>LexDAO</i>
         </a>
       </Text>
+      <Text direction={"column"} fontSize="xs">
+        <a href="https://bold-thing-f55.notion.site/KaliCo-Terms-of-Service-91ef53c0763d423dbc035a29dabc4ca9" target="_blank" rel="noreferrer">
+          <i>ToS</i>
+        </a>
+      </Text>
       <Spacer />
-      <Stack direction={"row"} spacing={4} border="1px solid" borderRadius="xl">
+      <Stack direction={"row"} spacing={-1} border="1px solid" borderRadius="xl">
         <SocialButton href={"https://github.com/kalidao"}>
           <FiGithub />
         </SocialButton>
         <SocialButton href={"https://twitter.com/_KaliDAO"}>
           <FiTwitter />
+        </SocialButton>
+        <SocialButton href={"https://discord.com/invite/UKCS9ghzUE"}>
+          <FiUsers />
         </SocialButton>
       </Stack>
     </HStack>
