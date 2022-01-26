8fa2b4b83e1fc2fcc4b1308c6b1143617025c822
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-01-26 12:13:31 -0500

    Update Nav.js
    bump

--- components/structure/Nav.js
+++ components/structure/Nav.js
@@ -1,18 +1,20 @@
-import React, { Component, useState, useContext, useEffect } from "react";
-import { BrowserView, MobileView, isBrowser } from "react-device-detect";
-import AppContext from "../../context/AppContext";
-import Link from "next/link";
-import { Flex, Spacer, HStack } from "@chakra-ui/react";
-import NavRightContainer from "./NavRightContainer";
-import Hamburger from "./Hamburger";
-import Kali from "./Kali";
-import KaliMobile from "./KaliMobile";
-import Account from "./Account";
-import Chain from "./Chain";
+import React, { Component, useState, useContext, useEffect } from "react"
+import { BrowserView, MobileView, isBrowser } from "react-device-detect"
+import AppContext from "../../context/AppContext"
+import Link from "next/link"
+import { Flex, Spacer, HStack } from "@chakra-ui/react"
+import NavRightContainer from "./NavRightContainer"
+import Hamburger from "./Hamburger"
+import Kali from "./Kali"
+import KaliMobile from "./KaliMobile"
+import Account from "./Account"
+import Chain from "./Chain"
+import DraftDoc from "../tools/DraftDoc"
 
 export default function HomeNav() {
-  const value = useContext(AppContext);
-  const { account, chainId } = value.state;
+  const value = useContext(AppContext)
+  const { web3, account, chainId } = value.state
+
   return (
     <HStack
       minH="10vh"
@@ -22,7 +24,8 @@ export default function HomeNav() {
     >
       {isBrowser == true ? <Kali /> : null}
       <Spacer />
+      <DraftDoc />
       <NavRightContainer />
     </HStack>
-  );
+  )
 }
