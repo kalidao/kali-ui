ea89ce3d85cb0a43ff091b650642d30c5c5888eb
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-01-30 13:04:20 -0500

    🥢tribute update (#34)
    * Update addresses.js
    
    🥢bump tribute
    
    * Update KaliDAOtribute.json
    
    🥢bump abi

--- constants/addresses.js
+++ constants/addresses.js
@@ -5,7 +5,7 @@ export const addresses =
     4: {
       factory: "0xF33B0D384440985E137A19822B84521e5D1378F5",
       extensions: {
-        tribute: "0x0b7012ef8ddd92BA381D06659b6Ed88e22cAe047",
+        tribute: "0x279b956fF82E93c68eDd16a65a8aEC7Db5CbeE0B",
         crowdsale: "0x4a2E86911Ad13Cd08A99Ba78b4097E66C71696fa",
         redemption: "0x4973670f38c98126F4cA1d74A92F7FE397d18F58",
       },
