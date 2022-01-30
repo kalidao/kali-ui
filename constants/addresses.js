cf886f9e20746811dd2b6cbbfc621617e5307713
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-01-30 12:52:08 -0500

    Update addresses.js
    🥢bump tribute

--- constants/addresses.js
+++ constants/addresses.js
@@ -5,7 +5,7 @@ export const addresses =
     4: {
       factory: "0xe581Ab5C2821a92155d1123b06a0081D262568f0",
       extensions: {
-        tribute: "0x0b7012ef8ddd92BA381D06659b6Ed88e22cAe047",
+        tribute: "0x279b956fF82E93c68eDd16a65a8aEC7Db5CbeE0B",
         crowdsale: "0x4a2E86911Ad13Cd08A99Ba78b4097E66C71696fa",
         redemption: "0x4973670f38c98126F4cA1d74A92F7FE397d18F58",
       },
