46804064a47cc25b2cff2f2256aa42e951e7f0bc
audsssy <stamfordh@hotmail.com>
2022-02-01 21:47:09 -0500

    Update
    * FactoryWrapper.js
    - Update default network to 4 (aka Rinkeby)
    
    * ChooseNetwork.js
    - Display "Next" button only after user is connected to Rinkeby

--- components/home/FactoryWrapper.js
+++ components/home/FactoryWrapper.js
@@ -12,7 +12,7 @@ import StepProgressBar from "./StepProgressBar";
 export default function FactoryWrapper() {
   const [visible, setVisible] = useState(0);
   const [details, setDetails] = useState({
-    network: 999,
+    network: 4,
     identity: {
       daoName: null,
       symbol: null,
