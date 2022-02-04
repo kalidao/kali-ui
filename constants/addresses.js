b708c20b84044cb8cdb0533bbecededbdfa24b1b
nerderlyne <shivanshityagi@icloud.com>
2022-02-05 03:20:06 +0530

    update addresses

--- constants/addresses.js
+++ constants/addresses.js
@@ -1,13 +1,20 @@
 export const addresses =
   // organized by chainId
-
   {
-    42161: {
-      factory: "0x6fcc42233f909a46EB006A7bd6BfE9D94928e64a",
+    1: {
+      factory: "0xdaB793D8F47476a7b66BdAA5230635fABD527d47",
       extensions: {
         tribute: "0x279b956fF82E93c68eDd16a65a8aEC7Db5CbeE0B",
         crowdsale: "0x6564704626aFa561B29D3973b39d46462FFFb04f",
         redemption: "0x4973670f38c98126F4cA1d74A92F7FE397d18F58",
       },
-    }
+    },
+    42161: {
+      factory: "0x6fcc42233f909a46EB006A7bd6BfE9D94928e64a",
+      extensions: {
+        tribute: null,
+        crowdsale: null,
+        redemption: null,
+      },
+    },
   };
