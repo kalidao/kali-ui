8df6df3c65d773265ed749bc6bd28796d93529ea
nerderlyne <shivanshityagi@icloud.com>
2022-02-05 00:40:55 +0530

    💄 extensions

--- components/dashboard/Extensions.js
+++ components/dashboard/Extensions.js
@@ -1,22 +1,57 @@
 import { useState, useContext, useEffect } from "react";
 import AppContext from "../../context/AppContext";
-import { Text, UnorderedList, ListItem } from "@chakra-ui/react";
+import {
+  Text,
+  UnorderedList,
+  ListItem,
+  HStack,
+  Spacer,
+} from "@chakra-ui/react";
+import DashedDivider from "../elements/DashedDivider";
+import InfoTip from "../elements/InfoTip";
+
+const ExtensionCard = ({ key, name }) => {
+  const assignLabel = (name) => {
+    let label;
+    if (name === "crowdsale")
+      return (label = "Sell membership in ETH or designated token");
+    if (name === "tribute")
+      return (label =
+        "Burn token to claim fair share of DAO capital aka ragequit");
+    if (name === "redemption")
+      return (label =
+        "Make a proposal with ETH, token or NFT as membership tribute");
+    else return null;
+  };
+
+  return (
+    <>
+      <HStack>
+        <Text>{name.replace(/^\w/, (s) => s.toUpperCase())}</Text>
+        <Spacer />
+        <InfoTip label={assignLabel(name)} />
+      </HStack>
+
+      <DashedDivider />
+    </>
+  );
+};
 
 export default function Extensions() {
   const value = useContext(AppContext);
   const { dao } = value.state;
 
-  return(
+  return (
     <>
-    {dao['extensions'] == null ? "No extensions installed" :
-    <>
-    <UnorderedList>
-      {Object.entries(dao["extensions"]).map(([k, v]) =>
-        <ListItem key={k}>{k}</ListItem>
+      {dao["extensions"] == null ? (
+        "No extensions installed"
+      ) : (
+        <>
+          {Object.entries(dao["extensions"]).map(([name, key]) => (
+            <ExtensionCard name={name} />
+          ))}
+        </>
       )}
-    </UnorderedList>
-    </>
-    }
     </>
   );
 }
