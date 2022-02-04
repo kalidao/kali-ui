5ca452274773680f6a1a82800eeb61ff7e9bc2c0
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-02-04 16:28:19 -0500

    Update addresses.js
    🚀 arbis

--- constants/addresses.js
+++ constants/addresses.js
@@ -2,8 +2,8 @@ export const addresses =
   // organized by chainId
 
   {
-    4: {
-      factory: "0xdaB793D8F47476a7b66BdAA5230635fABD527d47",
+    42161: {
+      factory: "0x6fcc42233f909a46EB006A7bd6BfE9D94928e64a",
       extensions: {
         tribute: "0x279b956fF82E93c68eDd16a65a8aEC7Db5CbeE0B",
         crowdsale: "0x6564704626aFa561B29D3973b39d46462FFFb04f",
