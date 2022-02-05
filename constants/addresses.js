fe932a8cd8d1a0d731f0305e67f9a399ce0dbfa3
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-02-05 17:20:05 -0500

    Update addresses.js (#68)
    add ricardian address

--- constants/addresses.js
+++ constants/addresses.js
@@ -3,6 +3,7 @@ export const addresses =
   {
     4: {
       factory: "0x58AC6a1Dd37b374Eb98FE751fc64502bB04Fb80f",
+      ricardian: "0x612a519C2ba312420c972D7a9714FaF27f4358DF",
       extensions: {
         tribute: "0xd62AB72CC6b53D98eed510646a69B21b77ce5A56",
         crowdsale: "0x276dD025512C3DB05FD224C0aF7d0ba3cc371EfB",
@@ -12,6 +13,7 @@ export const addresses =
     },
     42161: {
       factory: "0x6fcc42233f909a46EB006A7bd6BfE9D94928e64a",
+      ricardian: "0x02D77B4Ae9e1b2AE629055Ac0f71A29D13b29587",
       extensions: {
         tribute: "0xf1D291A527281049e66ABD3A41624B13D962868f",
         crowdsale: "0xC59596a20876F145504230f8c8B17CadE73cBe0c",
