41cd447e0b1a73bddef856fe9ee5180a6116037a
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-02-05 12:12:38 -0500

    Update addresses.js
    update redemptions

--- constants/addresses.js
+++ constants/addresses.js
@@ -6,7 +6,7 @@ export const addresses =
       extensions: {
         tribute: "0xd62AB72CC6b53D98eed510646a69B21b77ce5A56",
         crowdsale: "0x276dD025512C3DB05FD224C0aF7d0ba3cc371EfB",
-        redemption: "0x4973670f38c98126F4cA1d74A92F7FE397d18F58",
+        redemption: "0x11f44975e1B204E50108Af6BCB6539798cb15F75",
         // access: "0x3467B663F6e6B962f4f24aa5dB55e659D108Db9e",
       },
     },
@@ -15,7 +15,7 @@ export const addresses =
       extensions: {
         tribute: "0xf1D291A527281049e66ABD3A41624B13D962868f",
         crowdsale: "0xC59596a20876F145504230f8c8B17CadE73cBe0c",
-        redemption: null,
+        redemption: "0x7452BDCED6f344e0E4e1169377D369aDE1cB0Ca0",
         // access: "0x89832E0DbE3600a7358F2E3eA2d7aF5dc7d76E0C",
       },
     },
