906385ef0feed8830e920f769c28d0c636e0132a
Jordan Teague <ejteague@gmail.com>
2022-02-04 23:11:12 -0500

    updated params to mirror what's on main; fix vercel error

--- constants/supportedChains.js
+++ constants/supportedChains.js
@@ -1,17 +1,41 @@
 export const supportedChains = [
   {
     chainId: 4,
+    hexChain: '0x4',
     name: "rinkeby",
-    infura: "rinkeby.infura.io"
+    infura: "rinkeby.infura.io",
+    nativeCurrency: {
+      name: 'Rinkeby Ether',
+      symbol: 'rETH',
+      decimals: 18
+    },
+    rpcUrls: ['https://rinkeby-light.eth.linkpool.io'],
+    blockExplorerUrls: ['https://rinkeby.etherscan.io']
   },
   {
     chainId: 1,
+    hexChain: '0x1',
     name: "mainnet (Soon)",
-    infura: "mainnet.infura.io"
+    infura: "mainnet.infura.io",
+    nativeCurrency: {
+      name: 'Ether',
+      symbol: 'ETH',
+      decimals: 18
+    },
+    rpcUrls: ['https://main-light.eth.linkpool.io'],
+    blockExplorerUrls: ['https://etherscan.io']
   },
   {
     chainId: 42161,
-    name: "arbitrum (Soon)",
-    infura: "arbitrum-mainnet.infura.io"
+    hexChain: '0xa4b1',
+    name: "arbitrum",
+    infura: "arbitrum-mainnet.infura.io",
+    nativeCurrency: {
+      name: "Arbitrum Ether",
+      symbol: "AETH",
+      decimals: 18
+    },
+    rpcUrls: ['https://arb1.arbitrum.io/rpc'],
+    blockExplorerUrls: ['https://arbiscan.io']
   }
 ]
