559821aa67c520f111a60269ae512da12dc73d89
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-02-05 10:17:51 -0500

    Update addresses.js
    rink trib

--- constants/addresses.js
+++ constants/addresses.js
@@ -4,7 +4,7 @@ export const addresses =
     4: {
       factory: "0x58AC6a1Dd37b374Eb98FE751fc64502bB04Fb80f",
       extensions: {
-        tribute: "0x279b956fF82E93c68eDd16a65a8aEC7Db5CbeE0B",
+        tribute: "0xd62AB72CC6b53D98eed510646a69B21b77ce5A56",
         crowdsale: "0x6564704626aFa561B29D3973b39d46462FFFb04f",
         redemption: "0x4973670f38c98126F4cA1d74A92F7FE397d18F58",
       },
