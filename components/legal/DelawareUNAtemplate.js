ec80f6aa0ccdee10f5714e1eabf634fb559ef19d
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-02-07 03:41:36 -0500

    Create DelawareUNAtemplate.js
    UNA template

---
+++ components/legal/DelawareUNAtemplate.js
@@ -0,0 +1,180 @@
+import React from "react"
+import { Page, Text, Document, StyleSheet } from "@react-pdf/renderer"
+
+// Styles
+const styles = StyleSheet.create({
+  body: {
+    paddingTop: 35,
+    paddingBottom: 65,
+    paddingHorizontal: 60,
+  },
+  heading1: {
+    fontSize: 12,
+    marginTop: 12,
+    textAlign: "center",
+    textDecoration: "underline",
+    textTransform: "capitalize",
+    fontFamily: "Times-Roman",
+  },
+  heading2: {
+    fontSize: 12,
+    marginHorizontal: 12,
+    textAlign: "center",
+    textTransform: "capitalize",
+    fontFamily: "Times-Roman",
+  },
+  heading3: {
+    fontSize: 12,
+    marginHorizontal: 12,
+    textAlign: "center",
+    fontFamily: "Times-Roman",
+  },
+  text: {
+    marginTop: 12,
+    marginHorizontal: 12,
+    fontSize: 12,
+    textAlign: "justify",
+    fontFamily: "Times-Roman",
+  },
+  numbered_list: {
+    marginTop: 12,
+    marginHorizontal: 12,
+    fontSize: 12,
+    textIndent: 30,
+    textAlign: "justify",
+    fontFamily: "Times-Roman",
+  },
+  bulleted_list: {
+    fontSize: 12,
+    marginBottom: 12,
+    textIndent: 30,
+    textAlign: "justify",
+    fontFamily: "Times-Roman",
+  },
+  pageNumber: {
+    position: "absolute",
+    fontSize: 10,
+    bottom: 30,
+    left: 0,
+    right: 0,
+    textAlign: "center",
+    fontFamily: "Times-Roman",
+  },
+})
+
+// Utility
+const Br = () => "\n"
+const Tab = () => "  "
+
+// Template
+const DelawareUNAtemplate = ({ name, chain }) => {
+  return (
+    <Document>
+      <Page style={styles.body}>{
+      Trustless } Unincorporated Nonprofit Association Agreement ("TUNAA")
+
+      DELAWARE LLC OPERATING AGREEMENT
+
+      {name}
+
+      Designated Blockchain: {chain}
+
+      # Article I - Organization
+
+**Section 1.1 - Status.** [[DAO Name]] (the "DAO") is a "nonprofit association" under the Delaware Uniform Unincorporated Nonprofit Association Act, Del. Code Ann. Tit. 6, §§ 1901-1916.
+
+The DAO is not intended to be, and shall not be deemed to be, a partnership.
+
+**Section 1.2 - Purposes.** The DAO is organized to carry out the missions stated in its [[membership portal]] (available at [[hyperlink]]), related websites or otherwise memorialized in a writing by the DAO. The DAO is not intended to be or become an entity required to register as an "investment company" as defined in Section 3(a)(1)(A) of the Investment Company Act of 1940, as amended.
+
+**Section 1.3 - Nondiscrimination.** The DAO shall not arbitrarily discriminate on the basis of race, nationality, religion, age, gender, sexual orientation, disability, political affiliation, or otherwise.  
+
+**Section 1.4 - Decentralized Autonomous Organization (DAO).** The DAO is an unincorporated association of individuals, corporations, statutory trusts, business trusts, estates, trusts, partnerships, limited liability companies, associations, joint ventures, or any other legal or commercial entity, many, if not all, of whom agree to join together for a common, nonprofit purpose. For the DAO, that purpose is encapsulated in its mission statement.
+
+# Article II - Membership
+
+**Section 2.1 - Eligibility** Membership in , as defined in Del. Code Ann. Tit. 6, § 1901, shall be voluntary and application open to any individual of at least 18 years of age whose purpose or presumed intent is to contribute, to build, and to use the services of the DAO and is willing to accept the responsibilities and terms of membership. 
+
+**Section 2.2 - Admission.** Membership shall be granted or denied [[to DAO applicants by vote upon the submission of required information on a prescribed application form, paying any required application fee, and contributing to the DAO, if any, all as determined by the membership. The contribution requirement may be waived in whole or in part for persons of low income.]] [[automatically to persons (as defined in Del. Code Ann. Tit. 6, § 1901) upon their lawful receipt of [[NFT MEMBERSHIP INTEREST]].]] The DAO is a decentralized network of members and may admit or deny for any arbitrary purpose or lack of purpose.
+
+All of the DAO governing members shall have their membership determined [[automatically]] upon [[lawful]] receipt of [[voting tokens]] secured on a public blockchain (“Cryptographic Units”, and such holders, “Members”). Cryptographic Units are used for participating in and improving the governance of the DAO through affirmative majority votes effectuted via the Designated Smart Contract (defined herein) (such process, “Cryptographic Consensus”). Once the admission requirements are met a prospective Member may be put up to a vote of the full membership or similar process enabled by the Designated Smart Contract. The DAO will consistently review, and if necessary, Members may make adjustments to the admission requirements based on their evolving needs and as registered in a successful vote through the Designated Smart Contract.
+
+**Section 2.3 - Rights.** The DAO's Members shall utilize the “Designated Smart Contract” (a smart contract deployed to the Designated Blockchain at the Designated Blockchain Address) as the exclusive method of holding, allocating rights and obligations among the Members, and spending, or otherwise distributing any Tokens that are DAO Property, of minting and issuing Cryptographic Units and holding and recording votes among the Members. The DAO may also utilize the Designated Smart Contract to administer and facilitate certain other arrangements and transactions involving the DAO, the Members and/or third parties.
+
+Members’ rights and responsibilities are controlled by the use of the Designated Smart Contact used to conduct its governance and activities of the DAO. Members will cast votes and carry out the decisions made on the Designated Smart Contract.  Cryptographic Units are held in Account Addresses, key paired wallets controlled by Members in a designated hexadecimal address (“Member Web3 Account”).
+
+**Section 2.4 - Responsibilities.** Each Member shall keep reasonably current in payment of any dues or membership fees and other financial obligations of membership, if applicable. Each Member shall notify the DAO of an e-mail address or other acceptable communication channel by which such Member may receive written or electronic materials required or permitted by this document or shall notify the DAO that such Member has no e-mail address and designate the mutually acceptable form of communication.
+
+If you have received any Cryptographic Units or are otherwise a Member, you consent and agree to become legally bound by this Agreement as both a participant in the DAO and more specifically a "DAO Member".
+
+**Section 2.5 - Limitations.**  Status as a DAO Member does not (and shall not be deemed to) create, and the DAO does not (and shall not be deemed to) include, any authority, right or power on the part of a DAo Member to act as the agent, representative or attorney of or otherwise act on behalf of the DAO or any other Member, to bind the DAO or any other Member to any Contract or Liability or to convey any DAO Property or any asset, right or property owned or held by or on behalf of the DAO or any Member. Without limiting the generality of the foregoing, no Member shall be deemed the partner of the DAO or any other Member solely in virute of being a Member. No Member shall state, purport, imply, hold out or represent to any person that such Member or any other Member has any such authority, right or power.
+
+To the maximum extent permitted by applicable law, no Member shall be (or shall be deemed to be) liable for any liability of the DAO or any other Member. This shall not (and shall not be deemed to) create or imply any obligation of the DAO or any Member to indemnify or compensate any Member from, or hold any Member harmless against, any Liabilities incurred by such DAO Member under any applicable law, in connection with the Member's participation in the DAO or otherwise.
+
+**Section 2.6 - Inactive status.** A Member who falls from good standing may have their membership revoked or suspended through a Guild Kick.  References herein to the rights and entitlements of Members shall be understood to refer only to Members in good standing.
+
+**Section 2.7 - Access to information.** Members shall have access to information concerning operational and financial affairs via the DAO's preferred treasury application of the organization.  Currently the DAO treasury can be viewed via Designated Smart Contract.  
+
+**Section 2.8 - Settlement of disputes.** In any dispute between the DAO and any of its Members or former Members which cannot be resolved through informal negotiation, it shall be the policy of the DAO to prefer the use of mediation whereby an impartial mediator may facilitate negotiations between the parties and assist them in developing a mutually acceptable settlement. Neither party with a grievance against the other shall have recourse to litigation until the matter is submitted to mediation and attempted to be resolved in good faith.  All Members agree that there is a preference to settle disputes amongst Members or between Members and the DAO via decentralized dispute mechanisms in smart contract protocols. 
+
+**Section  2.9 - Transferability of Membership.** Membership rights and interests may not be transferred except by an affirmative majority vote of Members.  Any attempted transfer contrary to this section shall be wholly void and shall confer no rights on the intended transferee and shall be cause (though none is needed) to burn the Cryptographic Units through a Guild Kick member removal procedure.
+
+**Section 2.10 - Withdrawal and Expulsion.** A member may withdraw at any time upon notice to the DAO by electronic writing to an appointed representative of the DAO or by public display to the DAO’s online coordination systems (including, but not limited to, Discord or Telegram). Withdrawal shall be effectuated through a vote to burn such withdrawaing DAO Member's Cryptographic Units or mechanisms otherwise authorized in the Designated Smart Contract. Any such withdrawal request will not be unreasonably denied and shall be deemed conclusively as the DAO Member’s intent to withdraw from the DAO. A Member may be expelled by the DAO through the Guild Kick procedure established in paragraph 2.6 of this document and adopted by the membership. Upon termination of membership, all rights and interests in the DAO shall cease except for rights to redemption of capital pursuant to Article V below (if any).
+
+# Article III - Meetings of Members
+
+**Section 3.1 - Meetings.** Meetings of members shall be described on a basis at the discretion of the Members. Typically, governance meetings are set on weekly cadence through online chats where parties agree to conduct such other business as may properly come before the meeting.
+
+**Section 3.2 - Special meetings.** Special meetings of members may be called by a group (the "DAO Advisory Group") designated by an affirmative vote of DAO Members in accordance with the governance procedures of the Designated Smart Contract.  The DAO Advisory Group is not required and may never be formed.  Creation and designation of the DAO Advisory Group will be approved via the Designated Smart Contract. 
+
+**Section 3.3 -Time and place.** The date, time and place of all meetings of the DAO Advisory Group shall be determined by the DAO Advisory Group or, in the event that the DAO Advisory Group fails to act, by a call for vote by the Members to be approved by the native governance processes to the Designated Smart Contract.
+
+**Section 3.4 - Notice.** Each Member is responsible for monitoring votes of concern on the Designated Smart Contract.  Notice of votes can be set up by DAO Members via the Designated Smart Contract.  Notices of meetings shall also be posted on the DAO’s official media outlets, including the DAO Member information roster, but the inadvertent failure to do so shall not affect the validity of the meeting. Any business conducted at a meeting of DAO Members other than that specified in the notice of the meeting shall be of an advisory nature only. 
+
+ # Article IV - Decentralized Governance
+
+**Section 4.1 - Powers and duties.** Except as to matters reserved to members by law or by this constitution, all powers to be exercised on behalf of the DAO shall be exercised by or under the authority of Members or such agents or designees approved by Members through Designated Smart Contract voting.
+
+# Article V - Fiscal and Miscellaneous Matters
+
+**Section 5.1 - Fiscal year.** The fiscal year of the DAO  shall be the calendar year beginning January 1st and ending December 31st.
+
+**Section 5.2 - Indemnification.** The DAO shall indemnify its directors, officers, employees, or agents as required under Delaware law, and may indemnify such persons as permitted under Delaware law, including its Members for acts that do not involve bad faith or intentional misconduct, including fraud. Indemnification payments shall be made on a priority basis but only in such increments and at such times as will not jeopardize the ability of the DAO to pay its other obligations as they become due.
+
+**Section 5.3 - Communication by electronic means.** Unless otherwise required by law or by agreement, any notice, vote, consent, petition, or other oral or written communication required or permitted can be delivered by electronic means, provided that, in the case where such communication expressly or impliedly requires the signature of the person submitting the communication, means are in place to reasonably assure the authenticity of the signature.
+
+# Article VI - Interpretation and Amendment of TUNAA
+
+**Section 6.1 - Interpretation.** The DAO Advisory Group (if formed via vote of Members) shall have the power to interpret this TUNAA, apply them to particular circumstances, and adopt policies in furtherance of them, provided that all such actions are reasonable and consistent.
+ 
+**Section 6.2 - Severability.** In the event that any provision of this TUNAA is determined to be invalid or unenforceable under any statute or rule of law, then such provision shall be deemed inoperative to such extent and shall be deemed modified to conform with such statute or rule of law without affecting the validity or enforceability of any other provision of this TUNAA.
+
+**Section 6.3 - Amendment.** This TUNAA may be amended by presenting the redlined version of the amendments at a meeting of members, and adopted by a vote or merge request as recorded by Cryptographic Consensus. As an alternative to achieving Cryptographic Consensus, any Member may timely protest a merge request made to the TUNAA that has not been adopted as an approved version by Members via the Designated Smart Contract.  
+
+# Article VII - Definitions
+
+(a) **“Account Address”** means a public key address on the Designated Blockchain Network that is uniquely associated with a single private key, and at which no smart contract has been deployed.
+
+(b) **“Consensus Rules”** means the rules for transaction validity, block validity and determination of the canonical blockchain that are embodied in the Designated Client.
+
+(c) **“Contract”** means any: (i) written, oral, implied by course of performance or otherwise or other agreement, contract, understanding, arrangement, settlement, instrument, warranty, license, insurance policy, benefit plan or legally binding commitment or undertaking; or (ii) any representation, statement, promise, commitment, undertaking, right or obligation that may be enforceable, or become subject to an Order directing performance thereof, based on equitable principles or doctrines such as estoppel, reliance, or quasi-contract.
+
+(d) **“DAO Property"** means any Token or other asset, right or property licensed to or on deposit with or owned, held, custodied, controlled or possessed by or on behalf of teh DAO, including any Token on deposit with or held, controlled, possessed by or on deposit with the Designated Smart Contract.
+
+(e) **“Designated Blockchain”** means at any given time, the version of the digital blockchain ledger that at least a majority of nodes running the Designated Blockchain Client on the Designated Blockchain Network recognize as canonical as of such time in accordance with the Consensus Rules. The initial Designated Blockchain shall be the [[Designated Blockchain]] blockchain as recognized by the Designated Blockchain Client on the Designated Blockchain Network.
+
+(f) **“Designated Blockchain Client”** means the blockchain software client designated as the “Designated Blockchain Client” by the Members. The initial Designated Blockchain Client shall be [[Designated Blockchain Client]], as it may be updated from time to time.
+
+(g) “Designated Blockchain Network” means the blockchain network designated as the “Designated Blockchain Network” by the Members. The initial Designated Blockchain Network shall be [[Designated Blockchain Network]] as recognized by the Designated Blockchain Client.
+
+(h) **“Designated Smart Contract”** means the smart contract deployed at address [[Designated Smart Contract Address]] on the Designated Blockchain associated with the Members and Cryptographic Units.
+
+(i) **“Liability”** means any debt, obligation, duty or liability of any nature (including any unknown, undisclosed, unmatured, unaccrued, unasserted, contingent, indirect, conditional, implied, vicarious, inchoate derivative, joint, several or secondary liability), regardless of whether such debt, obligation, duty or liability would be required to be disclosed on a balance sheet prepared in accordance with generally accepted accounting principles and regardless of whether such debt, obligation, duty or liability is immediately due and payable. To be “Liable” means to have, suffer, incur, be obligated for or be subject to a Liability.
+
+(j) **"Guild Kick"**  means removal from the membership via vote by Members upon their Cryptographic Units to destroy another Member’s Cryptographic Units through a burn function or other similar process.  Unless specifically provided otherwise, or in the Designated Smart Contract, a member subject to a Guild Kick shall not be entitled to any distribution or return of capital, funds, retains, etc.   
+      </Page>
+    </Document>
+  )
+}
+
+export default DelawareUNAtemplate
