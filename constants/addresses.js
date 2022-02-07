f72061f2302b528c0bf0e07bec859ad58bf9fad3
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-02-06 21:09:58 -0500

    Update addresses.js
    deploys

--- constants/addresses.js
+++ constants/addresses.js
@@ -6,7 +6,7 @@ export const addresses =
       ricardian: "0x1a49A558557c9Fcd5073FB6641d2dc32233cb5D9",
       extensions: {
         tribute: "0x561D9C4EdB64524556856133Bf7B5640dB904656",
-        crowdsale: "0x489Bf91E282BA00B35107f8958b38eA1aE3aAEfc",
+        crowdsale: "0xD76B629c528548582Af14c4D3a851830BB0c6978",
         redemption: "0x3c9eB1c92b4063e6B9fA0531bC8966D3a09565D0",
         // access: "0x7799b86Ada91a507b5cbDA03638116A26A86358d",
       },
@@ -16,7 +16,7 @@ export const addresses =
       ricardian: "0x612a519C2ba312420c972D7a9714FaF27f4358DF",
       extensions: {
         tribute: "0xd62AB72CC6b53D98eed510646a69B21b77ce5A56",
-        crowdsale: "0x276dD025512C3DB05FD224C0aF7d0ba3cc371EfB",
+        crowdsale: "0x30BF15b764A2A096c37f8c8E1b6b43D853db9a36",
         redemption: "0x11f44975e1B204E50108Af6BCB6539798cb15F75",
         // access: "0x3467B663F6e6B962f4f24aa5dB55e659D108Db9e",
       },
@@ -26,7 +26,7 @@ export const addresses =
       ricardian: "0x0731FF841f2eB3F8Fef7BE1B683C733C9f2150f5",
       extensions: {
         tribute: "0xAe3357E4D401495Cfe6e6022734E11293BC63dfb",
-        crowdsale: "0x380E305100cb46B6Cc8c8918a93230b47646a77D",
+        crowdsale: "0x0AE06840C05bf261B1798571696F58F9Ac3b3174",
         redemption: "0xC43AE97f12d979FCAe346E93b62d747963956d63",
         // access: "0x43Cc55157d3A4e51ab99CaD6189726D768d7A124",
       },
@@ -36,7 +36,7 @@ export const addresses =
       ricardian: "0x02D77B4Ae9e1b2AE629055Ac0f71A29D13b29587",
       extensions: {
         tribute: "0xf1D291A527281049e66ABD3A41624B13D962868f",
-        crowdsale: "0xC59596a20876F145504230f8c8B17CadE73cBe0c",
+        crowdsale: "0x0bb3F43533FBf16d69dBdccf6AaAef81acd76FAB",
         redemption: "0x7452BDCED6f344e0E4e1169377D369aDE1cB0Ca0",
         // access: "0x89832E0DbE3600a7358F2E3eA2d7aF5dc7d76E0C",
       },
