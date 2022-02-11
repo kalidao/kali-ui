aa40348047564b209bc47b589599d8eb8e876a54
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-02-11 17:30:48 -0500

    Update addresses.js
    add factory addresses and rearrange access list

--- constants/addresses.js
+++ constants/addresses.js
@@ -4,42 +4,45 @@ export const addresses =
     1: {
       factory: "0xA051591c52c1378bc4a34c57445D181deEF100f0",
       ricardian: "0x1a49A558557c9Fcd5073FB6641d2dc32233cb5D9",
+      erc20factory: "0x65B2fb10C3D759b8D5828f617d146c5Ec380Ed1D",
+      access: "0x7799b86Ada91a507b5cbDA03638116A26A86358d",
       extensions: {
         tribute: "0x561D9C4EdB64524556856133Bf7B5640dB904656",
         crowdsale: "0xD76B629c528548582Af14c4D3a851830BB0c6978",
         redemption: "0x3c9eB1c92b4063e6B9fA0531bC8966D3a09565D0",
-        // access: "0x7799b86Ada91a507b5cbDA03638116A26A86358d",
       },
     },
     4: {
       factory: "0x58AC6a1Dd37b374Eb98FE751fc64502bB04Fb80f",
       ricardian: "0x612a519C2ba312420c972D7a9714FaF27f4358DF",
-      erc20factory: "0x6C15225260150fEc6E329dacc70b23e5DDA9436D",
+      erc20factory: "0x8a23526eDe4d7222150C3dFEFC47902180A19323",
+      access: "0x3467B663F6e6B962f4f24aa5dB55e659D108Db9e",
       extensions: {
         tribute: "0xd62AB72CC6b53D98eed510646a69B21b77ce5A56",
         crowdsale: "0x30BF15b764A2A096c37f8c8E1b6b43D853db9a36",
         redemption: "0x11f44975e1B204E50108Af6BCB6539798cb15F75",
-        // access: "0x3467B663F6e6B962f4f24aa5dB55e659D108Db9e",
       },
     },
     137: {
       factory: "0x3f15724d409444743613AdaFA4A4a4B49D9Ab2F9",
       ricardian: "0x0731FF841f2eB3F8Fef7BE1B683C733C9f2150f5",
+      erc20factory: "0xf8468a8A356B1b9DB2E918DbC1Ca31D597c9E54F",
+      access: "0x43Cc55157d3A4e51ab99CaD6189726D768d7A124",
       extensions: {
         tribute: "0xAe3357E4D401495Cfe6e6022734E11293BC63dfb",
         crowdsale: "0x0AE06840C05bf261B1798571696F58F9Ac3b3174",
         redemption: "0xC43AE97f12d979FCAe346E93b62d747963956d63",
-        // access: "0x43Cc55157d3A4e51ab99CaD6189726D768d7A124",
       },
     },
     42161: {
       factory: "0x6fcc42233f909a46EB006A7bd6BfE9D94928e64a",
       ricardian: "0x02D77B4Ae9e1b2AE629055Ac0f71A29D13b29587",
+      erc20factory: "0x5eeDa4696062Eaa319DDE62d2D350f4aBF0C88F4",
+      access: "0x89832E0DbE3600a7358F2E3eA2d7aF5dc7d76E0C",
       extensions: {
         tribute: "0xf1D291A527281049e66ABD3A41624B13D962868f",
         crowdsale: "0x0bb3F43533FBf16d69dBdccf6AaAef81acd76FAB",
         redemption: "0x7452BDCED6f344e0E4e1169377D369aDE1cB0Ca0",
-        // access: "0x89832E0DbE3600a7358F2E3eA2d7aF5dc7d76E0C",
       },
     },
   };
