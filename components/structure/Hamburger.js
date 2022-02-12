4149a8148cdcbe9b281014b200ce972992423021
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-02-11 22:44:34 -0500

    Update Hamburger.js
    rearrange hamburger

--- components/structure/Hamburger.js
+++ components/structure/Hamburger.js
@@ -26,11 +26,6 @@ export default function Hamburger() {
             <MenuItem color="kali.800">My DAOs</MenuItem>
           </a>
         </Link>
-        <Link href="https://www.lexdao.coop/#/directory">
-        <a>
-          <MenuItem color="kali.800">LexDAO Directory</MenuItem>
-        </a>
-        </Link>
         <a href="https://docs.kalidao.xyz/faq/" target="_blank" rel="noreferrer">
           <MenuItem color="kali.800">FAQs</MenuItem>
         </a>
@@ -40,6 +35,11 @@ export default function Hamburger() {
         <a href="https://discord.com/invite/UKCS9ghzUE" target="_blank" rel="noreferrer">
           <MenuItem color="kali.800">Support</MenuItem>
         </a>
+        <Link href="https://www.lexdao.coop/#/directory">
+        <a>
+          <MenuItem color="kali.800">LexDAO Directory</MenuItem>
+        </a>
+        </Link>
       </MenuList>
     </Menu>
   );
