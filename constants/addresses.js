4a758872f9fa52d7b9c877fec5a679e299207c89
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-02-11 19:16:13 -0500

    Update addresses.js
    🏭 add NFT minter

--- constants/addresses.js
+++ constants/addresses.js
@@ -5,6 +5,7 @@ export const addresses =
       factory: "0xA051591c52c1378bc4a34c57445D181deEF100f0",
       ricardian: "0x1a49A558557c9Fcd5073FB6641d2dc32233cb5D9",
       erc20factory: "0x65B2fb10C3D759b8D5828f617d146c5Ec380Ed1D",
+      nft: "0x9E7351cA95099496bcAC598Fe4d5201D2B1f9A8d",
       access: "0x7799b86Ada91a507b5cbDA03638116A26A86358d",
       extensions: {
         tribute: "0x561D9C4EdB64524556856133Bf7B5640dB904656",
@@ -16,6 +17,7 @@ export const addresses =
       factory: "0x58AC6a1Dd37b374Eb98FE751fc64502bB04Fb80f",
       ricardian: "0x612a519C2ba312420c972D7a9714FaF27f4358DF",
       erc20factory: "0x8a23526eDe4d7222150C3dFEFC47902180A19323",
+      nft: "0xbC208705aF624bE6AE63B27Cf1b94dbD1d7156d0",
       access: "0x3467B663F6e6B962f4f24aa5dB55e659D108Db9e",
       extensions: {
         tribute: "0xd62AB72CC6b53D98eed510646a69B21b77ce5A56",
@@ -27,6 +29,7 @@ export const addresses =
       factory: "0x3f15724d409444743613AdaFA4A4a4B49D9Ab2F9",
       ricardian: "0x0731FF841f2eB3F8Fef7BE1B683C733C9f2150f5",
       erc20factory: "0xf8468a8A356B1b9DB2E918DbC1Ca31D597c9E54F",
+      nft: "0x9BD8D05c793dc5ba044Fe4656b25092b269DfB5D",
       access: "0x43Cc55157d3A4e51ab99CaD6189726D768d7A124",
       extensions: {
         tribute: "0xAe3357E4D401495Cfe6e6022734E11293BC63dfb",
@@ -38,6 +41,7 @@ export const addresses =
       factory: "0x6fcc42233f909a46EB006A7bd6BfE9D94928e64a",
       ricardian: "0x02D77B4Ae9e1b2AE629055Ac0f71A29D13b29587",
       erc20factory: "0x5eeDa4696062Eaa319DDE62d2D350f4aBF0C88F4",
+      nft: "0x212CD0DE0C28858Fc72F792bb0F49d3B98d4cd0b",
       access: "0x89832E0DbE3600a7358F2E3eA2d7aF5dc7d76E0C",
       extensions: {
         tribute: "0xf1D291A527281049e66ABD3A41624B13D962868f",
