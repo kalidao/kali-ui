35e3d01c4333748edb6fac79b9927ed6ac905410
Jordan Teague <ejteague@gmail.com>
2022-02-13 10:16:21 -0500

    Create graph.js

---
+++ constants/graph.js
@@ -0,0 +1,5 @@
+export const graph = {
+  1: "https://api.thegraph.com/subgraphs/name/nerderlyne/kali-mainnet",
+  42161: "https://api.thegraph.com/subgraphs/name/nerderlyne/kali-arbitrum",
+  4: "https://api.thegraph.com/subgraphs/name/nerderlyne/kali-rinkeby",
+};
