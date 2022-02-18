beefc444817663a019bd95ac4af973916843598a
Shiv <shivanshityagi@icloud.com>
2022-02-18 16:39:42 +0530

    Update graph.js

--- constants/graph.js
+++ constants/graph.js
@@ -1,6 +1,6 @@
 export const graph = {
   1: "https://api.thegraph.com/subgraphs/name/nerderlyne/kali-mainnet",
   42161: "https://api.thegraph.com/subgraphs/name/nerderlyne/kali-arbitrum",
-  4: "https://api.studio.thegraph.com/query/21449/kali-rinkeby/v0.0.1",
+  4: "https://api.thegraph.com/subgraphs/name/nerderlyne/kali-rinkeby",
   137: "https://api.thegraph.com/subgraphs/name/nerderlyne/kali-matic",
 };
