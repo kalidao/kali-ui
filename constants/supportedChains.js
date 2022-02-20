b2c6536c60949acf22a7fe1a9e84cfa506a1853d
Jordan Teague <ejteague@gmail.com>
2022-02-19 17:31:38 -0700

    reduced block call size for poly

--- constants/supportedChains.js
+++ constants/supportedChains.js
@@ -1,58 +1,58 @@
 export const supportedChains = [
   {
     chainId: 1,
-    hexChain: '0x1',
+    hexChain: "0x1",
     name: "Ethereum",
     infura: "mainnet.infura.io",
     nativeCurrency: {
-      name: 'Ether',
-      symbol: 'ETH',
-      decimals: 18
+      name: "Ether",
+      symbol: "ETH",
+      decimals: 18,
     },
-    rpcUrls: ['https://main-light.eth.linkpool.io'],
-    blockExplorerUrls: ['https://etherscan.io'],
-    maxBlocks: null
+    rpcUrls: ["https://main-light.eth.linkpool.io"],
+    blockExplorerUrls: ["https://etherscan.io"],
+    maxBlocks: null,
   },
   {
     chainId: 42161,
-    hexChain: '0xa4b1',
+    hexChain: "0xa4b1",
     name: "Arbitrum",
     infura: "arbitrum-mainnet.infura.io",
     nativeCurrency: {
       name: "Arbitrum Ether",
       symbol: "AETH",
-      decimals: 18
+      decimals: 18,
     },
-    rpcUrls: ['https://arb1.arbitrum.io/rpc'],
-    blockExplorerUrls: ['https://arbiscan.io'],
-    maxBlocks: 20000
+    rpcUrls: ["https://arb1.arbitrum.io/rpc"],
+    blockExplorerUrls: ["https://arbiscan.io"],
+    maxBlocks: 20000,
   },
   {
     chainId: 137,
-    hexChain: '0x89',
+    hexChain: "0x89",
     name: "Polygon",
     infura: "polygon-mainnet.infura.io",
     nativeCurrency: {
       name: "Polygon",
       symbol: "MATIC",
-      decimals: 18
+      decimals: 18,
     },
-    rpcUrls: ['https://polygon-rpc.com'],
-    blockExplorerUrls: ['https://polygonscan.com'],
-    maxBlocks: 10000
+    rpcUrls: ["https://polygon-rpc.com"],
+    blockExplorerUrls: ["https://polygonscan.com"],
+    maxBlocks: 3000,
   },
   {
     chainId: 4,
-    hexChain: '0x4',
+    hexChain: "0x4",
     name: "Rinkeby (testnet)",
     infura: "rinkeby.infura.io",
     nativeCurrency: {
-      name: 'Rinkeby Ether',
-      symbol: 'rETH',
-      decimals: 18
+      name: "Rinkeby Ether",
+      symbol: "rETH",
+      decimals: 18,
     },
-    rpcUrls: ['https://rinkeby-light.eth.linkpool.io'],
-    blockExplorerUrls: ['https://rinkeby.etherscan.io'],
-    maxBlocks: null
-  }
-]
+    rpcUrls: ["https://rinkeby-light.eth.linkpool.io"],
+    blockExplorerUrls: ["https://rinkeby.etherscan.io"],
+    maxBlocks: null,
+  },
+];
