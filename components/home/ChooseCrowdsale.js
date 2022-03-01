30bf424c95389ece29d3dc56a62ebfb17d541aa4
nerderlyne <shivanshityagi@icloud.com>
2022-03-01 18:07:12 +0530

    crowdsale custom

--- components/home/ChooseCrowdsale.js
+++ components/home/ChooseCrowdsale.js
@@ -6,10 +6,14 @@ import {
   VStack,
   Switch,
   Spacer,
+  Button,
+  Input,
 } from "@chakra-ui/react";
 import ReactDatePicker from "react-datepicker";
 import InfoTip from "../elements/InfoTip";
 import DateSelect from "../elements/DateSelect";
+import Slider from "../elements/Slider";
+import Select from "../elements/Select";
 
 function ChooseCrowdsale({ details, setDetails }) {
   const [crowdsale, setCrowdsale] = useState(
@@ -26,6 +30,53 @@ function ChooseCrowdsale({ details, setDetails }) {
   const [listId, setListId] = useState(
     Boolean(details["extensions"]["crowdsale"]["listId"])
   );
+
+  const [purchaseMultiplier, setPurchaseMultiplier] = useState(1);
+  const [showSlider, setShowSlider] = useState(false);
+
+  const [purchaseToken, setPurchaseToken] = useState(0);
+  const [showCustomToken, setCustomToken] = useState(false);
+
+  const handlePurchaseToken = (e) => {
+    let token = e.target.value;
+    switch (token) {
+      case "0":
+        details["extensions"]["crowdsale"]["purchaseToken"] =
+          "0x000000000000000000000000000000000000dEaD";
+        break;
+      case "333":
+        setCustomToken(true);
+        break;
+    }
+    setDetails(details);
+  };
+
+  const handleCustomToken = (e) => {
+    const token = e.target.value;
+    console.log("token", token);
+    details["extensions"]["crowdsale"]["purchaseToken"] = token;
+    setDetails(details);
+  };
+
+  const handlePurchaseLimit = (e) => {
+    const limit = e.target.value;
+    details["extensions"]["crowdsale"]["purchaseLimit"] = limit;
+    setDetails(details);
+  };
+
+  useEffect(() => {
+    details["extensions"]["crowdsale"]["purchaseMultiplier"] =
+      purchaseMultiplier;
+    setDetails(details);
+  }, [purchaseMultiplier]);
+
+  const presentSlider = () => {
+    if (!showSlider) {
+      setShowSlider(true);
+    } else {
+      setShowSlider(false);
+    }
+  };
 
   useEffect(() => {
     details["extensions"]["crowdsale"]["active"] = crowdsale;
@@ -51,9 +102,9 @@ function ChooseCrowdsale({ details, setDetails }) {
       </HStack>
       {crowdsale ? (
         <>
-          <VStack>
-            <Text fontSize="sm" htmlFor="saleEnds">
-              When should the crowdsale end?
+          <HStack>
+            <Text fontSize="md" htmlFor="saleEnds">
+              Sale Ends Date
             </Text>
             <ReactDatePicker
               id="date-picker"
@@ -65,10 +116,28 @@ function ChooseCrowdsale({ details, setDetails }) {
               timeCaption="time"
               dateFormat="MMMM d, yyyy h:mm aa"
             />
+          </HStack>
+          <VStack w={"100%"} spacing="8" align="flex-start">
+            <HStack w={"100%"}>
+              <label htmlFor="purchaseToken">Purchase Token</label>
+              <InfoTip label="This token will be used to purchase from the sale" />
+              <Spacer />
+              <Select id="purchaseToken" onChange={handlePurchaseToken}>
+                <option value="0">ETH</option>
+                <option value="333">Custom</option>
+              </Select>
+            </HStack>
+            {showCustomToken && (
+              <Input
+                id="purchaseToken"
+                placeholder="Enter Token Address"
+                onChange={handleCustomToken}
+              />
+            )}
           </VStack>
           <HStack w={"100%"}>
-            <Text fontSize="sm" htmlFor="listId">
-              Add our accredited investors whitelist?
+            <Text fontSize="md" htmlFor="listId">
+              Accredited Investor Whitelist
             </Text>
             <InfoTip
               label={"Turning this on limits the crowdsale to Dos Commas"}
@@ -76,12 +145,53 @@ function ChooseCrowdsale({ details, setDetails }) {
             <Spacer />
             <Switch
               id="listId"
-              size="sm"
+              size="md"
               colorScheme="red"
               defaultChecked={listId}
               onChange={() => setListId(!listId)}
             />
           </HStack>
+          <VStack w={"100%"} spacing="8" align="flex-start">
+            <HStack w={"100%"}>
+              <label htmlFor="purchaseMultiplier">Purchase Mulitplier</label>
+              <InfoTip
+                label={"This determines the shares bought per purchase token"}
+              />
+              <Spacer />
+              <Button
+                h={"100%"}
+                bg="clear"
+                border="0px"
+                fontWeight="normal"
+                _hover={{ bg: "red" }}
+                onClick={() => presentSlider()}
+                color="white"
+              >
+                {purchaseMultiplier}%
+              </Button>
+            </HStack>
+            {showSlider && (
+              <Slider
+                id="purchaseMultiplier"
+                min={1}
+                max={255}
+                defaultValue={10}
+                marks={[50, 100, 150, 200]}
+                label="Slide purchase multiplier"
+                onChangeEnd={(v) => setPurchaseMultiplier(v)}
+              />
+            )}
+          </VStack>
+          {/* <HStack w={"100%"}>
+            <label htmlFor="purchaseLimit">Purchase Limit</label>
+            <InfoTip label="This limit the number of tokens that can be purchased by an account" />
+            <Spacer />
+            <Input
+              id="purchaseLimit"
+              placeholder="1000"
+              onChange={handlePurchaseLimit}
+            />
+          </HStack> */}
         </>
       ) : null}
     </>
