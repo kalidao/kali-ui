5642ef11d1a0a698030e6e7b5512af42c9a8c0d1
nerderlyne <shivanshityagi@icloud.com>
2022-03-13 00:52:45 +0530

    glass supreme

--- components/newproposal/NewProposal.js
+++ components/newproposal/NewProposal.js
@@ -17,7 +17,7 @@ import {
   Container,
   Spacer,
   Wrap,
-  WrapItem
+  WrapItem,
 } from "@chakra-ui/react";
 import { BrowserView, MobileView } from "react-device-detect";
 import { newProposalHelper } from "../../constants/newProposalHelper";
@@ -26,34 +26,44 @@ import { BiEdit } from "react-icons/bi";
 export default function NewProposal(props) {
   const [menuItem, setMenuItem] = useState(999); // arbitrary number where no proposal type is selected. if changed, must change below, too
   const value = useContext(AppContext);
-  const { web3, loading, account, abi, address, dao, chainId, visibleView, remount } = value.state;
+  const {
+    web3,
+    loading,
+    account,
+    abi,
+    address,
+    dao,
+    chainId,
+    visibleView,
+    remount,
+  } = value.state;
   const balances = props.balances;
   console.log("account", account);
 
   useEffect(() => {
-    console.log("testtt")
+    console.log("testtt");
     setMenuItem(999);
   }, [remount]);
 
   const ProposalTile = (props) => {
     return (
       <WrapItem>
-      <LinkBox
-        className="proposal-type-tile gradient-item"
-        w={{sm: '350px', md: '225px', lg: '200px', xl: '180px', '2xl': '180px'}}
-        h={{sm: '175px', md: '175px', lg: '200px', xl: '200px', '2xl': '200px'}}
-      >
-        <LinkOverlay href="#" onClick={() => setMenuItem(props.id)}>
-          <HStack>
-            <Icon as={props.icon} boxSize={9} p={2} rounded={5} border="1px solid white" />
-            <Spacer />
-          </HStack>
-          <Heading>
-            {props.title}
-          </Heading>
-        </LinkOverlay>
-        <Text>{props.description}</Text>
-      </LinkBox>
+        <LinkBox className="proposal-type-tile glass" minWidth="350px">
+          <LinkOverlay href="#" onClick={() => setMenuItem(props.id)}>
+            <HStack>
+              <Icon
+                as={props.icon}
+                boxSize={9}
+                p={2}
+                rounded={5}
+                border="1px solid white"
+              />
+              <Spacer />
+            </HStack>
+            <Heading>{props.title}</Heading>
+          </LinkOverlay>
+          <Text>{props.description}</Text>
+        </LinkBox>
       </WrapItem>
     );
   };
@@ -65,40 +75,38 @@ export default function NewProposal(props) {
 
   return (
     <>
-    <HStack>
-      <Icon as={BiEdit} w={10} h={10} className="h1-icon" />
-      <Heading as="h1">New Proposal</Heading>
-    </HStack>
+      <HStack mb="2vh">
+        <Icon as={BiEdit} w={10} h={10} className="h1-icon" />
+        <Heading as="h1">New Proposal</Heading>
+      </HStack>
       {dao == null ? null : account == null ? (
-        <Box className="gradient-item dashboard-tile" mt={10} color="white">
-        <Text mb={5}>Please connect your account to start making proposals!</Text>
-        <Button
-          className="transparent-btn"
-          onClick={value.connect}
-          border="none"
-        >
-          Connect
-        </Button>
+        <Box className="glass dashboard-tile" mt={10} color="white">
+          <Text mb={5}>
+            Please connect your account to start making proposals!
+          </Text>
+          <Button
+            className="transparent-btn"
+            onClick={value.connect}
+            border="none"
+          >
+            Connect
+          </Button>
         </Box>
       ) : (
         <>
-            {menuItem < 999 ? (
-              null
-            ) : (
-              <Wrap
-              >
-                {Object.entries(newProposalHelper).map(([k, v]) =>
-                    <ProposalTile
-                      key={`propTile-${k}`}
-                      id={k}
-                      title={newProposalHelper[k]["title"]}
-                      description={newProposalHelper[k]["description"]}
-                      icon={newProposalHelper[k]["icon"]}
-                    />
-                )}
-              </Wrap>
-            )}
-
+          {menuItem < 999 ? null : (
+            <Wrap>
+              {Object.entries(newProposalHelper).map(([k, v]) => (
+                <ProposalTile
+                  key={`propTile-${k}`}
+                  id={k}
+                  title={newProposalHelper[k]["title"]}
+                  description={newProposalHelper[k]["description"]}
+                  icon={newProposalHelper[k]["icon"]}
+                />
+              ))}
+            </Wrap>
+          )}
 
           {Object.entries(newProposalHelper).map(([k, v]) =>
             menuItem == k ? (
