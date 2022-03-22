d0b4bc5697cf66da071aabb8a4a7d494e4a272d8
audsssy <stamfordh@hotmail.com>
2022-03-21 23:26:28 -0400

    Update
    Tribute.js
    Update ERC20 tributes with big number
    
    ProcessModule.js
    Add logic to process tribute proposals

--- components/proposals/ProcessModule.js
+++ components/proposals/ProcessModule.js
@@ -2,10 +2,12 @@ import { useContext, useState } from "react";
 import AppContext from "../../context/AppContext";
 import { Input, Button } from "@chakra-ui/react";
 import { createToast } from "../../utils/toast";
+import tribAbi from "../../abi/KaliDAOtribute.json";
 
 export default function ProcessModule(props) {
   const value = useContext(AppContext);
-  const { web3, loading, account, abi, address } = value.state;
+  const { web3, loading, account, abi, address, dao } = value.state;
+  const tribAddress = dao["extensions"]["tribute"]["address"];
   const p = props["p"];
   const i = props["i"];
   var disabled = true;
@@ -17,29 +19,44 @@ export default function ProcessModule(props) {
     event.preventDefault();
     value.setLoading(true);
 
-    try {
-      let object = event.target;
-      var array = [];
-      for (let i = 0; i < object.length; i++) {
-        array[object[i].name] = object[i].value;
-      }
-
-      const { dao, id } = array;
-
+    if (p["proposer"] === tribAddress) {
       try {
-        const instance = new web3.eth.Contract(abi, address);
-
-        let result = await instance.methods
-          .processProposal(id)
+        const tribContract = new web3.eth.Contract(tribAbi, tribAddress);
+        // console.log(p["id"].toString(), dao["address"], account, tribAddress);
+        let result = await tribContract.methods
+          .releaseTributeProposalAndProcess(dao["address"], p["id"])
           .send({ from: account });
+        console.log(
+          "This is result from processing tribute proposal - ",
+          result
+        );
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      try {
+        let object = event.target;
+        var array = [];
+        for (let i = 0; i < object.length; i++) {
+          array[object[i].name] = object[i].value;
+        }
 
+        const { dao, id } = array;
+
+        try {
+          const instance = new web3.eth.Contract(abi, address);
+
+          let result = await instance.methods
+            .processProposal(id)
+            .send({ from: account });
+        } catch (e) {
+          value.toast(e);
+          value.setLoading(false);
+        }
       } catch (e) {
         value.toast(e);
         value.setLoading(false);
       }
-    } catch(e) {
-      value.toast(e);
-      value.setLoading(false);
     }
 
     value.setLoading(false);
@@ -50,7 +67,9 @@ export default function ProcessModule(props) {
       <Input type="hidden" name="dao" />
       <Input type="hidden" name="id" value={p["id"]} />
       {i == 0 || p["proposalType"] == 9 ? (
-        <Button type="submit" className="transparent-btn">Process</Button>
+        <Button type="submit" className="transparent-btn">
+          Process
+        </Button>
       ) : (
         <Button type="submit" className="transparent-btn" disabled>
           In Queue
