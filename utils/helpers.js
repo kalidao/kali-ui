8a5a153df94efe8d1c1ba3f1f7ae2fe13209c086
audsssy <stamfordh@hotmail.com>
2022-04-14 05:02:51 +0800

    Summary
    Add logic to (1) multiselect members for removal and (2) add notes or files with removal prop

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,6 @@
-// helper functions to assemble final `proposals` and `dao` objects
+import fleek from "@fleekhq/fleek-storage-js";
 
+// helper functions to assemble final `proposals` and `dao` objects
 // votingPeriod
 export const calculateVotingPeriod = (period, unit) => {
   let seconds;
@@ -13,3 +14,25 @@ export const calculateVotingPeriod = (period, unit) => {
   console.log(seconds);
   return seconds;
 };
+
+// helper function to upload files to IPFS via Fleek
+export async function uploadIpfs(dao, content, attachment) {
+  const input = {
+    apiKey: process.env.NEXT_PUBLIC_FLEEK_API_KEY,
+    apiSecret: process.env.NEXT_PUBLIC_FLEEK_API_SECRET,
+    bucket: "f4a2a9f1-7442-4cf2-8b0e-106f14be163b-bucket",
+    key: "DAO (" + dao + ")'s " + content + " is attached with "  + attachment.name,
+    data: attachment,
+    httpUploadProgressCallback: (event) => {
+      console.log(Math.round((event.loaded / event.total) * 100) + "% done");
+    },
+  };
+
+  try {
+    const result = await fleek.upload(input);
+    console.log("Image hash from Fleek: " + result.hash);
+    return result.hash
+  } catch (e) {
+    console.log(e);
+  }
+}
