e9722f2bcc17108a9a8b189ef434f58244017b58
audsssy <stamfordh@hotmail.com>
2022-04-26 15:18:48 +0800

    Update SendToken.js
    - UI update
    - Add Notes section

--- components/newproposal/SendToken.js
+++ components/newproposal/SendToken.js
@@ -16,17 +16,27 @@ import {
   Spacer,
   Stack,
   IconButton,
+  Text,
+  Box,
 } from "@chakra-ui/react";
 import NumInputField from "../elements/NumInputField";
 import { useForm, Controller, useFieldArray } from "react-hook-form";
 import { toDecimals, fromDecimals } from "../../utils/formatters";
 import { tokens } from "../../constants/tokens";
-import { AiOutlineDelete } from "react-icons/ai";
+import { AiOutlineDelete, AiOutlineUserAdd } from "react-icons/ai";
+import ProposalDescription from "../elements/ProposalDescription";
+import { validateEns } from "../tools/ensHelpers";
+import { uploadIpfs } from "../tools/ipfsHelpers";
 
 export default function SendToken() {
   const value = useContext(AppContext);
   const { web3, loading, account, abi, address, dao, chainId } = value.state;
   const [selectedOptions, setSelectedOptions] = useState([]);
+
+  // For Notes section
+  const [doc, setDoc] = useState([]);
+  const [note, setNote] = useState(null);
+  const [file, setFile] = useState(null);
 
   const handleSelect = (select) => {
     let value = select.target.value;
@@ -50,37 +60,47 @@ export default function SendToken() {
   });
 
   useEffect(() => {
-    append({ address: "" }); // add first recipient input field
+    append({ address: "" });
   }, [append]);
 
   const submitProposal = async (values) => {
-    //event.preventDefault();
     value.setLoading(true);
-    console.log("event", event);
 
-    try {
-      var { description_, recipients } = values; // this must contain any inputs from custom forms
-      console.log("values", values);
+      var { recipients } = values;
 
+      // Configure proposal type 
       const proposalType_ = 2;
 
-      let amounts_ = [];
+      // Configure description param and upload to IPFS if necessary
+      let description;
+      (note && file) ? description = await uploadIpfs(dao.address, "Mint Proposal", file.name) : (description = "none");
+      (note) ? description = note : description = "none";
+      (file) ? description = await uploadIpfs(dao.address, "Mint Proposal", file.name) : null;
 
+      // Configure accounts param and validate address or ENS
+      let recipients_ = []; 
+      let accounts_ = []; // For CALL
       for (let i = 0; i < recipients.length; i++) {
-        // voters ENS check
-        if (recipients[i].address.slice(-4) === ".eth") {
-          recipients[i].address = await web3.eth.ens
-            .getAddress(recipients[i].address)
-            .catch(() => {
-              value.toast(recipients[i].address + " is not a valid ENS.");
-              value.setLoading(false);
-            });
-        }
-
-        console.log(recipients[i].address);
-        if (recipients[i].address === undefined) {
+        const account = await validateEns(recipients[i].address, web3, value)
+        if (account === undefined) {
+          value.setLoading(false);
           return;
         }
+        recipients_.push(account)
+
+        // Replace accounts with token address to CALL token transfers via payload
+        let address_;
+        if (selectedOptions[i] == "ETH") {
+          address_ = account;
+        } else {
+          address_ = tokens[chainId][selectedOptions[i]]["address"];
+        }
+        accounts_.push(address_);
+      }
+
+      // Configure token amounts param
+      let amounts_ = [];
+      for (let i = 0; i < recipients.length; i++) {
         let element = document.getElementById(`recipients.${i}.share`);
         let value_ = element.value;
 
@@ -90,35 +110,23 @@ export default function SendToken() {
           amounts_.push(0);
         }
       }
-      console.log("Amounts Array", amounts_);
 
-      let accounts_ = [];
-      for (let i = 0; i < recipients.length; i++) {
-        let address_;
-        if (selectedOptions[i] == "ETH") {
-          address_ = recipients[i].address;
-        } else {
-          address_ = tokens[chainId][selectedOptions[i]]["address"];
-        }
-        accounts_.push(address_);
-      }
-      console.log("Tokens Array", accounts_);
-
+      // Configure payloads param
       let payloads_ = [];
       for (let i = 0; i < recipients.length; i++) {
         if (selectedOptions[i] == "ETH") {
           payloads_.push("0x");
         } else {
-          const ierc20 = require("../../abi/ERC20.json");
-          let address_ = tokens[chainId][selectedOptions[i]]["address"];
-          let decimals = tokens[chainId][selectedOptions[i]]["decimals"];
+          const tokenAbi = require("../../abi/ERC20.json");
+          let tokenAddress = tokens[chainId][selectedOptions[i]]["address"];
+          // let decimals = tokens[chainId][selectedOptions[i]]["decimals"];
           let element = document.getElementById(`recipients.${i}.share`);
           let value_ = element.value;
 
-          const tokenContract = new web3.eth.Contract(ierc20, address_);
+          const tokenContract = new web3.eth.Contract(tokenAbi, tokenAddress);
           var payload_ = tokenContract.methods
             .transfer(
-              recipients[i].address,
+              recipients_[i],
               toDecimals(
                 value_,
                 tokens[chainId][selectedOptions[i]]["decimals"]
@@ -129,59 +137,46 @@ export default function SendToken() {
         }
       }
 
-      console.log(payloads_);
-
-      const instance = new web3.eth.Contract(abi, address);
-
+      console.log(proposalType_, description, recipients_, accounts_, amounts_, payloads_);
       try {
+        const instance = new web3.eth.Contract(abi, address);
         let result = await instance.methods
-          .propose(proposalType_, description_, accounts_, amounts_, payloads_)
+          .propose(proposalType_, description, accounts_, amounts_, payloads_)
           .send({ from: account });
         value.setVisibleView(2);
       } catch (e) {
         value.toast(e);
         value.setLoading(false);
       }
-    } catch (e) {
-      value.toast(e);
-      value.setLoading(false);
-    }
 
     value.setLoading(false);
   };
 
   return (
     <form onSubmit={handleSubmit(submitProposal)}>
-      <VStack width="100%">
-        <Controller
-          name="description_"
-          control={control}
-          render={({ field }) => (
-            <FormControl>
-              <FormLabel htmlFor="description_">Description</FormLabel>
-              <Textarea
-                placeholder=". . ."
-                {...field}
-                {...register(`description_`, {
-                  required: "Please enter a description.",
-                })}
-              />
-            </FormControl>
-          )}
-        />
-
-        <List spacing={2} width="100%">
+      <VStack width="100%" align="flex-start">
+        <HStack w="100%">
+          <Text fontSize="14px">Send ETH or ERC20 tokens to the following addresses</Text>
+          <Spacer />
+          <Button
+            border="0px"
+            variant="ghost"
+            _hover={{
+              background: "green.400",
+            }}
+            onClick={() => append({ address: "" })}
+          >
+            <AiOutlineUserAdd color="white" />
+          </Button>
+        </HStack>
+        <Box h={"2%"} />
+        <List width={"100%"} spacing={3}>
           {fields.map((recipient, index) => (
             <ListItem
               display="flex"
               flexDirection="row"
               alignContent="center"
-              justifyContent="center"
               key={recipient.id}
-              className="glass"
-              p="10px 20px"
-              borderRadius="2xl"
-              spacing="2"
             >
               <HStack w="100%" spacing={4}>
                 <Stack w="60%">
@@ -191,9 +186,9 @@ export default function SendToken() {
                     defaultValue={recipient.address}
                     render={({ field }) => (
                       <FormControl>
-                        <FormLabel htmlFor={`recipients.${index}.address`}>
+                        {/* <FormLabel htmlFor={`recipients.${index}.address`}>
                           Recipient {index + 1}
-                        </FormLabel>
+                        </FormLabel> */}
                         <Input
                           placeholder="0x address or ENS"
                           {...field}
@@ -212,9 +207,9 @@ export default function SendToken() {
                     defaultValue={recipient.token}
                     render={({ field }) => (
                       <FormControl>
-                        <FormLabel htmlFor={`recipients.${index}.token`}>
+                        {/* <FormLabel htmlFor={`recipients.${index}.token`}>
                           Token
-                        </FormLabel>
+                        </FormLabel> */}
                         <Select id={index} onChange={handleSelect}>
                           <option>Select</option>
                           <option value="ETH">ETH</option>
@@ -235,9 +230,9 @@ export default function SendToken() {
                     defaultValue={recipient.share}
                     render={({ field }) => (
                       <FormControl isRequired>
-                        <FormLabel htmlFor={`recipients.${index}.share`}>
+                        {/* <FormLabel htmlFor={`recipients.${index}.share`}>
                           Amount
-                        </FormLabel>
+                        </FormLabel> */}
                         <NumInputField
                           min="0.000000000000000001"
                           defaultValue="1"
@@ -250,8 +245,6 @@ export default function SendToken() {
                 <IconButton
                   className="delete-icon"
                   aria-label="delete recipient"
-                  mt={8}
-                  ml={2}
                   icon={<AiOutlineDelete />}
                   onClick={() => remove(index)}
                 />
@@ -259,15 +252,16 @@ export default function SendToken() {
             </ListItem>
           ))}
         </List>
-
-        <HStack width="100%">
-          <Spacer />
-          <Button className="solid-btn" onClick={() => append({ address: "" })}>
-            +Add Recipient
-          </Button>
-        </HStack>
-
-        <Button className="solid-btn" onClick={handleSubmit(submitProposal)}>
+        <br />
+        <ProposalDescription doc={doc} setDoc={setDoc} note={note} setNote={setNote} setFile={setFile} />
+      </VStack>
+      <br />
+      <VStack w={"100%"}>
+        <Button
+          className="solid-btn"
+          // onClick={handleSubmit(submitProposal)}
+          type="submit"
+        >
           Submit Proposal
         </Button>
       </VStack>
