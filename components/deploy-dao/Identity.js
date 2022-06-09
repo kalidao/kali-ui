a5558665c5b3aada2c2936c0e20b313dbeac20e8
Shiv <shivanshityagi@icloud.com>
2022-06-09 22:41:51 +0530

    add name error
    Signed-off-by: Shiv <shivanshityagi@icloud.com>

--- components/deploy-dao/Identity.js
+++ components/deploy-dao/Identity.js
@@ -1,21 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Flex, Button, Warning } from '../../styles/elements'
 import { Form, Label, Input } from '../../styles/form-elements'
 import { useForm } from 'react-hook-form'
 import { useStateMachine } from 'little-state-machine'
 import updateAction from './updateAction'
-
+import { useNetwork } from 'wagmi'
+import { getNames } from '../../graph/queries'
 export default function Identity({ setStep }) {
   const {
     register,
     handleSubmit,
+    setError,
     formState: { errors },
   } = useForm()
+  const { activeChain } = useNetwork()
   const { actions, state } = useStateMachine({ updateAction })
   const { hardMode } = state
+  const [names, setNames] = useState([])
+
+  useEffect(() => {
+    if (!activeChain) return
+    const fetchNames = async () => {
+      const result = await getNames(activeChain?.id)
+      const names = []
+      for (let i = 0; i < result?.data?.daos.length; i++) {
+        names.push(result?.data?.daos?.[i]?.token?.name)
+      }
+      setNames(names)
+    }
+
+    fetchNames()
+    return () => {
+      fetchNames()
+    }
+  }, [activeChain])
 
   const onSubmit = (data) => {
-    console.log(data)
+    // name check
+    if (names.includes(data?.name)) {
+      setError(
+        'name',
+        {
+          type: 'custom',
+          message: 'Name is not unique. Please choose another!',
+        },
+        {
+          shouldFocus: true,
+        },
+      )
+      return
+    }
     actions.updateAction(data)
 
     if (!hardMode) {
@@ -35,10 +69,10 @@ export default function Identity({ setStep }) {
             name="name"
             placeholder="KaliDAO"
             aria-invalid={errors.name ? 'true' : 'false'}
-            {...register('name', { required: true })}
+            {...register('name', { required: 'Name is required.' })}
             defaultValue={state.name}
           />
-          {errors.name && errors.name.type === 'required' && <span>Name is required.</span>}
+          {errors.name && <span>{errors?.name?.message}</span>}
         </Flex>
       </Flex>
       <Flex dir="row" align="separate">
--- graph/queries/index.js
+++ graph/queries/index.js
@@ -2,5 +2,6 @@ import { getDaoInfo } from './getDaoInfo'
 import { getMembers } from './getMembers'
 import { getProposals } from './getProposals'
 import { getProposal } from './getProposal'
+import { getNames } from './getNames'
 
-export { getDaoInfo, getMembers, getProposals, getProposal }
+export { getDaoInfo, getMembers, getProposals, getProposal, getNames }
