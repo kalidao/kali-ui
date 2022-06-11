2dc4cb093b22782a4391d05b166099f881bb4c2a
Shiv <shivanshityagi@icloud.com>
2022-06-11 21:06:57 +0530

    change proposal icon
    Signed-off-by: Shiv <shivanshityagi@icloud.com>

--- components/dao-dashboard/layout/sidebar/NewProposal.js
+++ components/dao-dashboard/layout/sidebar/NewProposal.js
@@ -1,13 +1,15 @@
 import { Dialog, DialogTrigger, DialogContent } from '../../../../styles/Dialog'
 import { Box } from '../../../../styles/elements'
 import { NewProposalModal } from '../../newproposal'
-
+import { FaPen } from 'react-icons/fa'
 export default function NewProposal() {
   // TODO: Disable when fetching
   return (
     <Dialog>
       <DialogTrigger>
-        <Box variant="create">+</Box>
+        <Box variant="create">
+          <FaPen size="25px" />
+        </Box>
       </DialogTrigger>
       <DialogContent>
         <NewProposalModal proposalProp={'menu'} />
