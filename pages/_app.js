8fd8cdde7cbab18f20217aa774e7fdfa37522153
Shiv <shivanshityagi@icloud.com>
2022-06-15 00:57:52 +0530

    update
    Signed-off-by: Shiv <shivanshityagi@icloud.com>

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ import { MoralisProvider } from 'react-moralis'
 import globalStyles from '../styles/globalStyles'
 
 const { chains, provider } = configureChains(
-  [chain.mainnet, chain.polygon, chain.arbitrum, chain.rinkeby, chain.optimism],
+  [chain.mainnet, chain.polygon, chain.arbitrum, chain.optimism, chain.rinkeby],
   [infuraProvider({ infuraId: process.env.NEXT_PUBLIC_INFURA_ID }), publicProvider()],
 )
 
