21de54bd4a531efe4cae23566ef0162a331f7356
audsssy <stamfordh@hotmail.com>
2022-06-15 21:29:00 +0800

    User Card 😉

---
+++ components/user-daos/UserCard.js
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react'
+import { styled } from '../../styles/stitches.config'
+import { truncateAddress } from '../../utils'
+import { Flex, Box, Text } from '../../styles/elements'
+import { getDaoChain } from '../../utils'
+import { useNetwork, useEnsName, useAccount } from 'wagmi'
+import { getRandomEmoji } from '../../utils'
+import { Avatar, AvatarImage, AvatarFallback } from '@radix-ui/react-avatar'
+
+const Name = styled('div', {
+  fontFamily: 'Bold',
+})
+
+const Address = styled('div', {
+  fontFamily: 'Screen',
+})
+
+// disable when not active chain
+export default function UserCard({ dao, chain }) {
+  const { activeChain } = useNetwork()
+  const { data: account } = useAccount()
+  const { data: ensName, isLoading } = useEnsName({
+    address: account.address,
+    chainId: activeChain.id,
+  })
+
+  useEffect(() => {
+    console.log(ensName)
+  }, [ensName])
+
+  return (
+    <Box as="a" variant="profile">
+      <Box
+        css={{
+          background: '$gray9',
+          borderRadius: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '30px',
+          width: '30px',
+        }}
+      >
+        {getRandomEmoji('0x123334423902309390230943')}
+      </Box>
+      <Flex dir="col" gap="sm">
+        {ensName ? <Text>{ensName}</Text> : <Text>{truncateAddress(account.address)}</Text>}
+        <Text>Hi</Text>
+        <Text>Hi</Text>
+      </Flex>
+    </Box>
+  )
+}
--- styles/elements/Box.js
+++ styles/elements/Box.js
@@ -79,6 +79,36 @@ const Box = styled('div', {
           border: '1px solid $mauve12',
         },
       },
+      profile: {
+        background: '$gray2',
+        color: '$gray11',
+        width: '10rem',
+        height: '100%',
+        padding: '2rem',
+
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+
+        gap: '2rem',
+        border: '1px solid $mauve10',
+        borderRadius: '20px',
+        lineHeight: '100%',
+        textDecoration: 'none',
+        overflow: 'hidden',
+
+        // '&:hover': {
+        //   background: '$gray3',
+        //   color: '$gray12',
+        //   border: '1px solid $mauve11',
+        // },
+        // '&:active': {
+        //   background: '$gray4',
+        //   color: '$gray12',
+        //   border: '1px solid $mauve12',
+        // },
+      },
     },
   },
 })
