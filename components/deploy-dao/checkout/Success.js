94884bec5bcf7dd0e3fcf5585b477da852f7c791
Shiv <shivanshityagi@icloud.com>
2022-06-17 03:03:18 +0530

    📧 entity registration
    Signed-off-by: Shiv <shivanshityagi@icloud.com>

--- components/deploy-dao/checkout/Success.js
+++ components/deploy-dao/checkout/Success.js
@@ -1,29 +1,58 @@
 import React from 'react'
 import { useContractEvent, useNetwork } from 'wagmi'
+import { useStateMachine } from 'little-state-machine'
 import { Box } from '../../../styles/elements'
+import { Spinner } from '../../elements'
 
 import { addresses } from '../../../constants/addresses'
 import FACTORY_ABI from '../../../abi/KaliDAOfactory.json'
 import { useRouter } from 'next/router'
+import { init, send } from 'emailjs-com'
+
+init(process.env.NEXT_PUBLIC_EMAIL_ID)
 
 export default function Success() {
+  const { state } = useStateMachine()
   const { activeChain } = useNetwork()
   const router = useRouter()
+
   useContractEvent(
     {
       addressOrName: activeChain?.id ? addresses[activeChain.id]['factory'] : AddressZero,
       contractInterface: FACTORY_ABI,
     },
     'DAOdeployed',
-    (event) =>
+    (event) => {
+      handleEmail(event)
       setTimeout(
         router.push(`/daos/${encodeURIComponent(activeChain?.id)}/${encodeURIComponent(event[0])}/info`),
-        10000,
-      ),
+        30000,
+      )
+    },
     {
       chainId: activeChain?.id,
     },
   )
 
-  return <div>Processing...</div>
+  const handleEmail = (data) => {
+    if (state.email && state.docType != 'none') {
+      const params = {
+        dao: data[0],
+        network: activeChain?.id,
+        email: state.email,
+        entity_type: state.docType,
+      }
+
+      send('default_service', 'template_c37vmuw', params).then(
+        function (response) {
+          console.log('EMAIL SUCCESS!', response.status, response.text)
+        },
+        function (error) {
+          console.log('EMAIL FAILED!', error)
+        },
+      )
+    }
+  }
+
+  return <Spinner />
 }
