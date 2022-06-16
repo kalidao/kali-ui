88bc5dd640f7b11feca4d603ab5ca2bb75086b43
Shiv <shivanshityagi@icloud.com>
2022-06-17 00:04:30 +0530

    💄 header
    Signed-off-by: Shiv <shivanshityagi@icloud.com>

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -8,7 +8,7 @@ const StyledHeader = styled(Flex, {
   // borderBottom: '1px solid $gray800',
   boxShadow:
     'rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset',
-  background: '$background',
+  background: '$gray1',
   width: '100%',
   position: 'fixed',
   left: '0',
