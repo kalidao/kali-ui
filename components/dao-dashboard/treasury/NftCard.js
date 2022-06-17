6c00f953e1ac6fbc4a75a0cb99e3a8e5c21b0485
Shiv <shivanshityagi@icloud.com>
2022-06-17 05:52:49 +0530

    🔥 update NFT
    Signed-off-by: Shiv <shivanshityagi@icloud.com>

--- components/dao-dashboard/treasury/NftCard.js
+++ components/dao-dashboard/treasury/NftCard.js
@@ -5,6 +5,8 @@ import { useMoralisWeb3Api } from 'react-moralis'
 import useSWR from 'swr'
 import { Flex, Box, Text } from '../../../styles/elements'
 import { Spinner } from '../../elements/'
+import { Dialog, DialogTrigger, DialogContent, DialogClose, DialogTitle } from '../../../styles/Dialog'
+import { ExternalLinkIcon } from '@radix-ui/react-icons'
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
@@ -17,19 +19,64 @@ export default function NftCard({ nft }) {
 
   console.log('nftMeta', data)
   return (
-    <Flex>
-      {data ? (
-        <Box
+    <Dialog>
+      <DialogTrigger>
+        {data ? (
+          <Box
+            css={{
+              height: '150px',
+              width: '150px',
+            }}
+          >
+            <Image src={data['image']} height="100%" width="100%" alt="NFT Image" />
+          </Box>
+        ) : (
+          <Spinner />
+        )}
+      </DialogTrigger>
+      <DialogContent>
+        <DialogClose asChild />
+        <DialogTitle
           css={{
-            height: '150px',
-            width: '150px',
+            fontFamily: 'Bold',
+          }}
+        >
+          {data['name']}
+        </DialogTitle>
+        <Flex
+          css={{
+            padding: '1rem',
+            gap: '0.5rem',
           }}
         >
           <Image src={data['image']} height="100%" width="100%" alt="NFT Image" />
-        </Box>
-      ) : (
-        <Spinner />
-      )}
-    </Flex>
+          <Flex gap="sm" dir="col" align="separate">
+            <Text
+              css={{
+                fontFamily: 'Regular',
+              }}
+            >
+              {data['description']}
+            </Text>
+            <Text
+              as="a"
+              css={{
+                fontFamily: 'Regular',
+                color: '$amber11',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'flex-start',
+                gap: '0.2rem',
+              }}
+              href={data['external_url']}
+              target="_blank"
+            >
+              External URL
+              <ExternalLinkIcon />
+            </Text>
+          </Flex>
+        </Flex>
+      </DialogContent>
+    </Dialog>
   )
 }
