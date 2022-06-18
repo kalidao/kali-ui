948cdcb014e2232dfb22c8292c3338398c1492fe
Shiv <shivanshityagi@icloud.com>
2022-06-19 01:44:59 +0530

    add create DAO button
    Signed-off-by: Shiv <shivanshityagi@icloud.com>

--- pages/lexy.js
+++ pages/lexy.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Layout from '../components/layout'
 import Lexy from '../components/lexy/index'
+import NewDaoSquare from '../components/my-daos/NewDaoSquare'
 
 export default function LexyPage() {
   return (
     <Layout heading="Lexy">
       <Lexy />
+      <NewDaoSquare />
     </Layout>
   )
 }
