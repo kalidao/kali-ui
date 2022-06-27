b989ad07971f54e03127c10aed7a7ae6961b698c
Shivanshi Tyagi <shivanshityagi@icloud.com>
2022-06-27 23:26:13 +0530

    🧹 no prop msg
    Signed-off-by: Shivanshi Tyagi <shivanshityagi@icloud.com>

--- components/dao-dashboard/proposal/post/index.js
+++ components/dao-dashboard/proposal/post/index.js
@@ -1,3 +1,4 @@
+import { Pencil1Icon } from '@radix-ui/react-icons'
 import React, { useMemo } from 'react'
 import { Flex, Text } from '../../../../styles/elements'
 import { ProposalCard } from './ProposalCard'
@@ -21,9 +22,20 @@ export default function Proposals({ proposals }) {
         Proposals
       </Text>
       <Flex dir="col">
-        {memoizedProposals.length > 0
-          ? memoizedProposals.map((proposal) => <ProposalCard key={proposal['id']} proposal={proposal} />)
-          : 'No proposals. Make one by clicking the + icon.'}
+        {memoizedProposals.length > 0 ? (
+          memoizedProposals.map((proposal) => <ProposalCard key={proposal['id']} proposal={proposal} />)
+        ) : (
+          <Flex gap="sm" align="center">
+            <Text
+              css={{
+                fontFamily: 'Regular',
+              }}
+            >
+              No proposals. Make one by clicking the pencil icon.
+            </Text>
+            <Pencil1Icon />
+          </Flex>
+        )}
       </Flex>
     </Flex>
   )
