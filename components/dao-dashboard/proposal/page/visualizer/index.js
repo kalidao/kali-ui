fdc0f5b6ee3f590f592427d3f707a1e83584502e
Shivanshi Tyagi <shivanshityagi@icloud.com>
2022-06-30 23:18:56 +0530

    add burn and escape visual
    Signed-off-by: Shivanshi Tyagi <shivanshityagi@icloud.com>

--- components/dao-dashboard/proposal/page/visualizer/index.js
+++ components/dao-dashboard/proposal/page/visualizer/index.js
@@ -16,6 +16,29 @@ export default function Visualizer({ proposal }) {
       )
     }
   }
+
+  const Burn = () => {
+    for (let i = 0; i < proposal?.accounts; i++) {
+      return (
+        <Flex gap="md">
+          <Text as="a" href={`https://app.kali.gg/users/${proposal?.accounts[i]}`}>
+            {truncateAddress(proposal?.accounts[i])}
+          </Text>
+          <Text>{ethers.utils.formatEther(proposal?.amounts[i])}</Text>
+        </Flex>
+      )
+    }
+  }
+
+  const Escape = () => {
+    return (
+      <Flex gap="md">
+        <Text>Killing Proposal #{proposal['amounts'][0]}</Text>
+      </Flex>
+    )
+  }
+
+  console.log(proposal)
   return (
     <>
       {proposal?.proposalType === 'MINT' && (
@@ -40,6 +63,54 @@ export default function Visualizer({ proposal }) {
             MINTING
           </Flex>
           <Mint />
+        </Flex>
+      )}
+      {proposal?.proposalType === 'BURN' && (
+        <Flex
+          dir="col"
+          gap="md"
+          css={{
+            border: '1px solid $gray6',
+            borderRadius: '20px',
+            padding: '1rem',
+          }}
+        >
+          <Flex
+            as="h3"
+            css={{
+              fontFamily: 'Regular',
+              fontWeight: '600',
+              fontSize: '16px',
+              color: '$gray12',
+            }}
+          >
+            Burning
+          </Flex>
+          <Burn />
+        </Flex>
+      )}
+      {proposal?.proposalType === 'ESCAPE' && (
+        <Flex
+          dir="col"
+          gap="md"
+          css={{
+            border: '1px solid $gray6',
+            borderRadius: '20px',
+            padding: '1rem',
+          }}
+        >
+          <Flex
+            as="h3"
+            css={{
+              fontFamily: 'Regular',
+              fontWeight: '600',
+              fontSize: '16px',
+              color: '$gray12',
+            }}
+          >
+            Escape
+          </Flex>
+          <Escape />
         </Flex>
       )}
     </>
