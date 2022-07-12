ab94888fa76f4eb790f7bffab7a9891e14063e7f
Daniel <95552251+d3centra@users.noreply.github.com>
2022-07-12 19:33:52 +0200

    Update Header.js

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -6,10 +6,9 @@ import { ConnectButton } from '@rainbow-me/rainbowkit'
 box-shadow: 0 1px 0 #04040533, 0 1.5px 0 #0606070d, 0 2px 0 #0404050d;
     border-bottom: 1px solid #a3a3ac1a;
 const StyledHeader = styled(Flex, {
-  // borderBottom: '1px solid $gray800',
+  borderBottom: '1px solid #a3a3ac1a',
   boxShadow:
     '0 1px 0 #04040533, 0 1.5px 0 #0606070d, 0 2px 0 #0404050d',
-  border-bottom: '1px solid #a3a3ac1a',
   background: '#36393f',
   width: '100%',
   position: 'fixed',
