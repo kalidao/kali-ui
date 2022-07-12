8d829b65c4f56032c0371543d53604b82c7df284
Daniel <95552251+d3centra@users.noreply.github.com>
2022-07-12 19:36:26 +0200

    Update Header.js

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -7,8 +7,7 @@ box-shadow: 0 1px 0 #04040533, 0 1.5px 0 #0606070d, 0 2px 0 #0404050d;
     border-bottom: 1px solid #a3a3ac1a;
 const StyledHeader = styled(Flex, {
   borderBottom: '1px solid #a3a3ac1a',
-  boxShadow:
-    '0 1px 0 #04040533, 0 1.5px 0 #0606070d, 0 2px 0 #0404050d',
+  boxShadow: '0 1px 0 #04040533, 0 1.5px 0 #0606070d, 0 2px 0 #0404050d',
   background: '#36393f',
   width: '100%',
   position: 'fixed',
