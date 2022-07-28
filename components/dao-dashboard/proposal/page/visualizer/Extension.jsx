2b4be1d940965b02eec2e20fb18f4e90c75f2782
shiv <shivanshityagi@icloud.com>
2022-07-28 19:06:02 +0000

    update extensions

--- components/dao-dashboard/proposal/page/visualizer/Extension.jsx
+++ components/dao-dashboard/proposal/page/visualizer/Extension.jsx
@@ -2,14 +2,29 @@ import { Flex, Text } from '../../../../../styles/elements'
 import { ethers } from 'ethers'
 import { useRouter } from 'next/router'
 import { addresses } from '../../../../../constants/addresses'
+import decodeExtensions from './decodeExtensions'
+import KALIDAO_ABI from '../../../../../abi/KaliDAO.json'
+import { useContractRead } from 'wagmi'
 
 export default function Extension({ accounts, amounts, payloads }) {
   const router = useRouter()
+  const { dao, chainId } = router.query
   const extensions = addresses[router.query.chainId]['extensions']
+  const { data: status, error } = useContractRead(
+    {
+      addressOrName: dao,
+      contractInterface: KALIDAO_ABI,
+    },
+    'extensions',
+    {
+      args: accounts[0],
+      chainId: Number(chainId),
+    },
+  )
 
   for (let i = 0; i < accounts.length; i++) {
-    // const decoded = decodeExtensions(accounts[i], payloads[i], router.query.chainId)
-    // console.log('decoded', decoded)
+    const decoded = decodeExtensions(accounts[i], payloads[i], chainId)
+    console.log(amounts, status, error)
     let extension
     for (const key in extensions) {
       if (accounts[i].toLowerCase() == extensions[key].toLowerCase()) {
@@ -17,16 +32,33 @@ export default function Extension({ accounts, amounts, payloads }) {
       }
     }
     return (
-      <Flex
-        css={{
-          fontFamily: 'Regular',
-          alignItems: 'center',
-          justifyContent: 'space-evenly',
-        }}
-      >
-        <Text>{extension}</Text>
-        <Text>{amounts[i] == 1 ? 'Activating' : 'Deactivating'}</Text>
-      </Flex>
+      <>
+        <Flex
+          css={{
+            fontFamily: 'Regular',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+        >
+          {/* TODO: Removing till I add activation to subgraph  */}
+          {/* <Text>{extension}</Text>
+        <Text>{amounts[i] != 0 ? (status == true ? 'Deactivating' : 'Activating') : 'Activating'}</Text> */}
+        </Flex>
+        {decoded &&
+          decoded['values'].map((item) => (
+            <Flex
+              css={{
+                fontFamily: 'Regular',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+              }}
+              key={item['label']}
+            >
+              <Text>{item['label']}</Text>
+              <Text>{item['value']}</Text>
+            </Flex>
+          ))}
+      </>
     )
   }
 }
--- components/dao-dashboard/proposal/page/visualizer/decodeExtensions.js
+++ components/dao-dashboard/proposal/page/visualizer/decodeExtensions.js
@@ -1,26 +1,40 @@
 import { ethers } from 'ethers'
-import CROWDSALE_ABI from '../../../../../abi/KaliDAOcrowdsaleV2.json'
-import REDEMPTION_ABI from '../../../../../abi/KaliDAOredemption.json'
-import TRIBUTE_ABI from '../../../../../abi/KaliDAOtribute.json'
 import { addresses } from '../../../../../constants/addresses'
+import { unixToDate } from '../../../../../utils/time'
 
-// TODO: issue with parsing payload for func sig
 const decodeExtensions = (address, payload, chainId) => {
-  console.log(
-    'payload {} value {}',
-    ethers.utils.hexStripZeros(payload),
-    chainId,
-    address,
-    addresses[chainId]['extensions'],
-  )
   const extensions = addresses[chainId]['extensions']
   for (const key in extensions) {
     if (address.toLowerCase() == extensions[key].toLowerCase()) {
-      console.log(`${key}: ${extensions[key]}`)
       try {
-        const iface = new ethers.utils.Interface(extensionsABI[key])
-        const tx = iface.parseTransaction({ data: payload, value: ethers.utils.parseEther('0') })
-        if (tx) return { type: key, tx: tx }
+        const decoded = ethers.utils.defaultAbiCoder.decode(extensionsHelper[key]['types'], payload)
+        if (decoded) {
+          let values = []
+          for (let i = 0; i < extensionsHelper[key]['types'].length; i++) {
+            let value = decoded[i]
+            if (extensionsHelper[key]['display'][i] === 'saleType') {
+              if (decoded[i].toString() == '0') value = 'Public'
+              else value = 'Private'
+            }
+            if (extensionsHelper[key]['display'][i] === 'date') {
+              value = unixToDate(value)
+            }
+            if (extensionsHelper[key]['display'][i] === 'BigNumber') {
+              value = ethers.utils.formatEther(value)
+            }
+            if (extensionsHelper[key]['display'][i] === 'token') {
+              if (value == ethers.constants.AddressZero) {
+                value = 'ETH'
+              }
+            }
+            values.push({
+              label: extensionsHelper[key]['labels'][i],
+              value: value,
+              display: extensionsHelper[key]['display'][i],
+            })
+          }
+          return { type: key, values: values }
+        }
       } catch (e) {
         console.error(e)
       }
@@ -28,10 +42,25 @@ const decodeExtensions = (address, payload, chainId) => {
   }
 }
 
-const extensionsABI = {
-  crowdsale2: CROWDSALE_ABI,
-  tribute: TRIBUTE_ABI,
-  redemption: REDEMPTION_ABI,
+const extensionsHelper = {
+  crowdsale2: {
+    labels: [
+      'Sale Type',
+      'Purchase Multiplier',
+      'Purchase Asset',
+      'Sale Ends',
+      'Purchase Limit',
+      'Personal Limit',
+      'Details',
+    ],
+    types: ['uint256', 'uint8', 'address', 'uint32', 'uint96', 'uint96', 'string'],
+    display: ['saleType', 'number', 'token', 'date', 'BigNumber', 'BigNumber', 'string'],
+  },
+  redemption: {
+    labels: ['Redeemable Tokens', 'Starts From'],
+    types: ['address[]', 'uint256'],
+    display: ['array', 'date'],
+  },
 }
 
 export default decodeExtensions
