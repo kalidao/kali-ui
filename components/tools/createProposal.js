8d478a03b3568b19d31ee3d0f5fb6bde5a75269b
shiv <shivanshityagi@icloud.com>
2022-08-02 23:56:52 +0000

    update createProposal

--- components/tools/createProposal.js
+++ components/tools/createProposal.js
@@ -1,9 +1,9 @@
 import fleek from '@fleekhq/fleek-storage-js'
-import fleekStorage from '@fleekhq/fleek-storage-js'
 
 // createProposal and upload
 export async function createProposal(dao, chainId, type, title, description) {
   const obj = {
+    contract: 'KaliV1',
     dao: dao,
     chainId: chainId,
     type: type,
@@ -16,7 +16,7 @@ export async function createProposal(dao, chainId, type, title, description) {
     apiSecret: process.env.NEXT_PUBLIC_FLEEK_API_SECRET,
     bucket: 'fa221543-b374-4588-8026-c2c9aefa4206-bucket',
     key: dao + 'proposal' + type,
-    data: JSON.stringify(obj),
+    data: JSON.stringify(obj, null, 2),
     httpUploadProgressCallback: (event) => {
       console.log(Math.round((event.loaded / event.total) * 100) + '% done')
     },
