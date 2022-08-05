60c957000d14654928079c5a6ce8fe6073fe19b5
audsssy <stamfordh@hotmail.com>
2022-08-05 22:44:54 +0800

    Update getMembers.js

--- graph/queries/getMembers.js
+++ graph/queries/getMembers.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { GRAPH_URL } from '../url'
 
 export const getMembers = async (chainId, address) => {
-  console.log('memberParms', chainId, address)
+  // console.log('memberParms', chainId, address)
   try {
     const res = await fetch(GRAPH_URL[chainId], {
       method: 'POST',
@@ -23,7 +23,6 @@ export const getMembers = async (chainId, address) => {
           }`,
       }),
     })
-    console.log('res', res)
     const data = await res.json()
     return data
   } catch (e) {
