00095877fa728944393d6e4614d65cc3c9a863b0
audsssy <stamfordh@hotmail.com>
2022-08-06 15:28:02 +0800

    use WAGMI to sign

--- components/dao-dashboard/proposal/vote/index.js
+++ components/dao-dashboard/proposal/vote/index.js
@@ -2,10 +2,10 @@ import React, { useCallback, useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { Box, Text } from '../../../../styles/elements'
 import { BsFillHandThumbsUpFill, BsFillHandThumbsDownFill } from 'react-icons/bs'
-import { useAccount, useSigner, useContractWrite, chain } from 'wagmi'
+import { useAccount, useSigner, useContractWrite, useContract, useSignTypedData } from 'wagmi'
 import DAO_ABI from '../../../../abi/KaliDAO.json'
 import { AddressZero } from '@ethersproject/constants'
-import { uploadVoteSignature, fetchVoteSignatures } from '../../../tools/ipfsHelpers'
+import { uploadVoteData, fetchVoteData } from '../../../tools/ipfsHelpers'
 import { getMembers } from '../../../../graph/queries'
 import { ethers } from 'ethers'
 
@@ -15,24 +15,24 @@ export default function Vote({ proposal }) {
   const chainId = router.query.chainId
   const [response, setResponse] = useState(null)
   const [members, setMembers] = useState(null)
-  // console.log(daoAddress, chainId)
+  const [voteApproval, setVoteApproval] = useState(null)
 
   // const votingPeriod = proposal['dao']['votingPeriod']
   // console.log('votingPeriod', votingPeriod)
   const { data: account } = useAccount()
   const { data: signer } = useSigner()
-  const { data: voteData, writeAsync: vote } = useContractWrite(
-    {
-      addressOrName: daoAddress ?? AddressZero,
-      contractInterface: DAO_ABI,
-    },
-    'vote',
-    {
-      onSuccess() {
-        console.log('vote', voteData)
-      },
-    },
-  )
+  // const { data: voteData, writeAsync: vote } = useContractWrite(
+  //   {
+  //     addressOrName: daoAddress ?? AddressZero,
+  //     contractInterface: DAO_ABI,
+  //   },
+  //   'vote',
+  //   {
+  //     onSuccess() {
+  //       console.log('vote', voteData)
+  //     },
+  //   },
+  // )
 
   const { data: voteBySigData, writeAsync: voteBySig } = useContractWrite(
     {
@@ -60,6 +60,35 @@ export default function Vote({ proposal }) {
     },
   )
 
+  const daoContract = useContract({
+    addressOrName: daoAddress ?? AddressZero,
+    contractInterface: DAO_ABI,
+  })
+
+  const { data: signedData, signTypedData } = useSignTypedData({
+    domain: {
+      name: 'KALI',
+      version: '1',
+      chainId: Number(chainId),
+      verifyingContract: daoAddress,
+    },
+    types: {
+      SignVote: [
+        { name: 'signer', type: 'address' },
+        { name: 'proposal', type: 'uint256' },
+        { name: 'approve', type: 'bool' },
+      ],
+    },
+    value: {
+      signer: account?.address,
+      proposal: Number(proposal['serial']),
+      approve: true,
+    },
+    onSettled(data, error) {
+      console.log('Settled', { data, error })
+    },
+  })
+
   useEffect(() => {
     let mounted = true
     const fetchMembers = async () => {
@@ -82,46 +111,20 @@ export default function Vote({ proposal }) {
           _members.push(member)
           setMembers([..._members])
         }
-        // if (members?.members[i].address.toLowerCase() === user?.address.toLowerCase()) {
-        // }
       }
     } else {
       console.log('members not found')
     }
-
-    // getVote()
   }, [response])
 
-  const sign = async () => {
-    const domain = {
-      name: 'KALI',
-      version: '1',
-      chainId: Number(chainId),
-      verifyingContract: daoAddress,
+  // Submit vote data to IPFS
+  useEffect(() => {
+    console.log(voteApproval)
+    if (voteApproval && signedData) {
+      uploadVoteData(daoAddress, Number(chainId), proposal['serial'], voteApproval, account?.address, signedData)
+      setVoteApproval(null)
     }
-
-    const types = {
-      SignVote: [
-        { name: 'signer', type: 'address' },
-        { name: 'proposal', type: 'uint256' },
-        { name: 'approve', type: 'bool' },
-      ],
-    }
-
-    const message = {
-      signer: account?.address,
-      proposal: Number(proposal['serial']),
-      approve: true,
-    }
-
-    try {
-      const signature = await signer._signTypedData(domain, types, message)
-      console.log(signer, domain, types, message, signature)
-      return signature
-    } catch (e) {
-      console.log(e)
-    }
-  }
+  }, [signedData])
 
   const left =
     new Date().getTime() - new Date(proposal?.dao?.votingPeriod * 1000 + proposal?.votingStarts * 1000).getTime()
@@ -130,8 +133,8 @@ export default function Vote({ proposal }) {
 
   const castVote = useCallback(
     async (approval) => {
-      const signature = await sign()
-      uploadVoteSignature(daoAddress, Number(chainId), proposal['serial'], approval, account?.address, signature)
+      signTypedData()
+      setVoteApproval(approval)
     },
     [account, proposal],
   )
@@ -139,33 +142,35 @@ export default function Vote({ proposal }) {
   const getVote = async () => {
     let votes
     if (members.length > 0) {
-      votes = await fetchVoteSignatures(daoAddress, proposal['serial'], members)
-      const votesData = getVoteDetail(votes)
+      votes = await fetchVoteData(daoAddress, proposal['serial'], members)
+
+      const votesData = await getVoteDetail(votes)
+
       console.log(votesData)
-      try {
-        const mc = await multicall({
-          args: [votesData],
-          overrides: {
-            gasLimit: 1050000,
-          },
-        })
-        console.log(mc)
-      } catch (e) {
-        console.log(e)
-      }
+
+      // try {
+      //   const mc = await multicall({
+      //     args: [votesData],
+      //     overrides: {
+      //       gasLimit: 1050000,
+      //     },
+      //   })
+      // } catch (e) {
+      //   console.log(e)
+      // }
     }
   }
 
-  const getVoteDetail = (votes) => {
+  const getVoteDetail = async (votes) => {
     let data = []
     console.log(votes)
     if (votes) {
       for (let i = 0; i < votes.length; i++) {
+        console.log(votes[i].signature)
         const { r, s, v } = ethers.utils.splitSignature(votes[i].signature)
 
         console.log([votes[i].member, Number(proposal['serial']), votes[i].approval, v, r, s])
-        const iface = new ethers.utils.Interface(DAO_ABI)
-        const data_ = iface.encodeFunctionData('voteBySig', [
+        const data_ = daoContract.interface.encodeFunctionData('voteBySig', [
           votes[i].member,
           Number(proposal['serial']),
           votes[i].approval,
