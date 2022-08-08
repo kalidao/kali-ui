1ae9f6d6fba01211a29ed9c4edce46a6d868ab9b
nerderlyne <nerderlyne@users.noreply.github.com>
2022-08-08 10:25:10 +0000

    Applying prettier

--- components/hooks/index.js
+++ components/hooks/index.js
@@ -7,4 +7,4 @@ import { useFetch } from './useFetch'
 import { useGraph } from './useGraph'
 import { useENS } from './useENS'
 
-export { useIsMounted, useFetch, useGraph, useENS}
+export { useIsMounted, useFetch, useGraph, useENS }
