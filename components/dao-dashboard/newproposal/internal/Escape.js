e2f07df925a85bd3d9c4008f486c0fddeb9739cf
audsssy <stamfordh@hotmail.com>
2022-08-17 15:38:34 -0400

    remove dead code

--- components/dao-dashboard/newproposal/internal/Escape.js
+++ components/dao-dashboard/newproposal/internal/Escape.js
@@ -22,8 +22,6 @@ export default function Escape({ kill, title, editor }) {
     },
     'propose',
   )
-  // Form
-  const [warning, setWarning] = useState(null)
 
   // TODO: Popup to change network if on different network from DAO
   const submit = async (e) => {
@@ -39,7 +37,6 @@ export default function Escape({ kill, title, editor }) {
     }
 
     console.log('Proposal Params - ', 10, docs, [AddressZero], [kill], [Array(0)])
-    if (proposalSelected != 0) {
       try {
         const tx = await writeAsync({
           args: [
@@ -57,10 +54,6 @@ export default function Escape({ kill, title, editor }) {
       } catch (e) {
         console.log('error', e)
       }
-      setWarning(null)
-    } else {
-      setWarning('There was an error in submitting this proposal.')
-    }
   }
 
   return (
@@ -84,7 +77,6 @@ export default function Escape({ kill, title, editor }) {
           <Label htmlFor="type">Proposal to Kill</Label>
           <Input disabled={true} defaultValue={kill} />
         </FormElement>
-        {warning && <Warning warning={warning} />}
         <Button onClick={submit}>Submit</Button>
       </Form>
     </Flex>
