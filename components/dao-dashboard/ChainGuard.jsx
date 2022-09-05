d63a1abfe312200e9a4a567fa00fc90707164496
shiv <shivanshityagi@icloud.com>
2022-09-05 16:49:17 +0000

    update component name

--- components/dao-dashboard/ChainGuard.jsx
+++ components/dao-dashboard/ChainGuard.jsx
@@ -4,8 +4,7 @@ import { Warning } from '../../styles/elements'
 import { useRouter } from 'next/router'
 import { Box } from '../../styles/elements'
 
-
-export default function SwitchChain({ children }) {
+export default function ChainGuard({ children }) {
   const router = useRouter()
   const daoAddress = router.query.dao
   const daoChainId = router.query.chainId
@@ -13,21 +12,13 @@ export default function SwitchChain({ children }) {
   console.log({ daoAddress, daoChainId })
 
   const { chain: userChain, chains } = useNetwork()
-  const daoChainName = chains?.find(chain => chain.id == daoChainId)?.name
+  const daoChainName = chains?.find((chain) => chain.id == daoChainId)?.name
   console.log({ chains, daoChainName })
 
-  const isWrongChain = (userChain?.id != daoChainId) ? true : false
-  const wrongChainWarning = userChain ?
-    `Your Web3 wallet is connected to ${userChain?.name}. You need to switch to ${daoChainName} - the native blockchain of this DAO in order to submit transactions to it.`
-    :
-    `Your Web3 wallet is disconnected. You need to connect to ${daoChainName} - the native blockchain of this DAO in order to submit transactions to it.`
+  const isWrongChain = userChain?.id != daoChainId ? true : false
+  const wrongChainWarning = userChain
+    ? `Your Web3 wallet is connected to ${userChain?.name}. You need to switch to ${daoChainName} - the native blockchain of this DAO in order to submit transactions to it.`
+    : `Your Web3 wallet is disconnected. You need to connect to ${daoChainName} - the native blockchain of this DAO in order to submit transactions to it.`
 
-  return (
-    (isWrongChain) ?
-      <Warning warning={wrongChainWarning} />
-      :
-      <Box>
-        {children}
-      </Box>
-  )
+  return isWrongChain ? <Warning warning={wrongChainWarning} /> : <Box>{children}</Box>
 }
