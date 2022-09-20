097e1a976b734dab9c7bdc5dbeba00c4af75a716
shiv <shivanshityagi@icloud.com>
2022-09-20 07:07:36 +0000

    update back button

--- pages/daos/[chainId]/[dao]/proposals/[proposalId]/index.js
+++ pages/daos/[chainId]/[dao]/proposals/[proposalId]/index.js
@@ -1,29 +1,24 @@
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useEffect } from 'react'
 import Layout from '../../../../../../components/dao-dashboard/layout'
 import ProposalView from '../../../../../../components/dao-dashboard/proposal/page'
 import { Flex } from '../../../../../../styles/elements'
 import { getProposal } from '../../../../../../graph/queries'
 import Back from '../../../../../../styles/proposal/Back'
 
-export const getServerSideProps = async (context) => {
-  const address = context.params.dao
-  const proposalId = context.params.proposalId
-  const chainId = context.params.chainId
-
-  const result = await getProposal(chainId, address, proposalId)
-
-  return {
-    props: {
-      proposal: result?.data?.proposals[0],
-    },
-  }
-}
 export default function ProposalPage({ proposal }) {
   const router = useRouter()
-  // const proposal = data && data['proposals'][0]
+  const { dao, chainId } = router.query
 
-  console.log('proposal data', router.query.dao, router.query.proposalId, proposal)
+  useEffect(() => {
+    router.prefetch(`/daos/${chainId}/${dao}`)
+  }, [])
+
+  const goBack = (e) => {
+    e.preventDefault()
+
+    router.push(`/daos/${chainId}/${dao}`)
+  }
 
   return (
     <Layout heading={`Proposal #${proposal?.serial}`} content="Discuss and vote on the proposal.">
@@ -37,9 +32,23 @@ export default function ProposalPage({ proposal }) {
           borderLeft: '1px solid hsla(0, 0%, 90%, 0.1)',
         }}
       >
-        <Back onClick={() => router.back()} />
+        <Back onClick={goBack} />
         <ProposalView proposal={proposal} />
       </Flex>
     </Layout>
   )
+}
+
+export const getServerSideProps = async (context) => {
+  const address = context.params.dao
+  const proposalId = context.params.proposalId
+  const chainId = context.params.chainId
+
+  const result = await getProposal(chainId, address, proposalId)
+
+  return {
+    props: {
+      proposal: result?.data?.proposals[0],
+    },
+  }
 }
