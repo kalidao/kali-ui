6e6550ba18bcdcefd1ed338787d6e8af9f2edca0
shiv <shivanshityagi@icloud.com>
2022-09-26 12:16:21 +0000

    tribute

--- pages/daos/[chainId]/[dao]/tribute.tsx
+++ pages/daos/[chainId]/[dao]/tribute.tsx
@@ -1,10 +1,9 @@
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import Layout from '@components/dao-dashboard/layout'
-import { Home } from '@components/dao-dashboard'
 import { useContractRead } from 'wagmi'
 import DAO_ABI from '@abi/KaliDAO.json'
-import { Heading, Stack, Text } from '@kalidao/reality'
+import { Heading, Stack, Text, Input } from '@kalidao/reality'
 
 const Tribute: NextPage = () => {
     const router = useRouter()
@@ -20,7 +19,7 @@ const Tribute: NextPage = () => {
         <Layout heading={data ? data.toString() : 'Dashboard'} content="Create or vote on a proposal.">
             <Stack>
                 <Heading>Make a Tribute</Heading>
-                <Input />
+                <Input label="Description" />
             </Stack>
         </Layout>
     )
