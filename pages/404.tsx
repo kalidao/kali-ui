f9fb4acc042a5b9f498ad30532b58f2bdfaebb29
Shiv <shivanshityagi@icloud.com>
2022-10-12 03:11:30 +0900

    remove tools

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,35 +1,15 @@
 import { NextPage } from 'next'
-import { Text, Flex, Box } from '@design/elements'
+import {Heading, Text, Stack, Box} from '@kalidao/reality'
+import Layout from '@components/layout'
 import Link from 'next/link'
 
 const Custom404: NextPage = () => {
-  return (
-    <Flex
-      css={{
-        minHeight: '100vh',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        gap: '5px',
-      }}
-    >
-      <h1>404 - Not Found</h1>
-      <Box
-        css={{
-          background: '$gray2',
-          height: '2px',
-          width: '25vw',
-          marginBottom: '10px',
-        }}
-      ></Box>
-      <Flex
-        css={{
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          gap: '5px',
-        }}
-      >
+  return (<Layout content="404 Page">
+    <Box style={{
+      minHeight: "90vh"
+    }} display={"flex"} alignItems="center" justifyContent={"center"}>
+      <Stack align="center" justify={"center"}>
+      <Heading>404 - Not Found</Heading>
         <Text>
           Tell us how you got here on our{' '}
           <a href="https://discord.com/invite/UKCS9ghzUE" target="_blank" rel="noreferrer">
@@ -49,8 +29,9 @@ const Custom404: NextPage = () => {
           </Link>
           .
         </Text>
-      </Flex>
-    </Flex>
+      </Stack>
+      </Box>
+    </Layout>
   )
 }
 
