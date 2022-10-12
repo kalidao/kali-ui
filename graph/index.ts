e44eaeb4ce84ddc7eb33a9a30ceb1f159923613c
Shiv <shivanshityagi@icloud.com>
2022-10-12 18:30:15 +0900

    update imports

--- graph/index.ts
+++ graph/index.ts
@@ -1,22 +1,7 @@
 import { GRAPH_URL } from './url'
-
-import { DAO_MEMBERS } from './dao-queries'
-import { DAO_PROPOSALS } from './dao-queries'
-import { CHECK_APPS } from './dao-queries'
-import { ALL_DAOS } from './dao-queries'
-import { FETCH_PROPOSAL } from './dao-queries'
-import { DAO_TOKEN } from './dao-queries'
-import { USER_DAOS } from './user-queries'
 import { WRAPPR_GRAPH_URL } from './url'
 
 export {
   GRAPH_URL,
-  WRAPPR_GRAPH_URL,
-  DAO_MEMBERS,
-  DAO_PROPOSALS,
-  CHECK_APPS,
-  FETCH_PROPOSAL,
-  ALL_DAOS,
-  DAO_TOKEN,
-  USER_DAOS,
+  WRAPPR_GRAPH_URL
 }
