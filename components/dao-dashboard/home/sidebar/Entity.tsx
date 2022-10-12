9cbc9af6c8dba5dcf9d27da167047775f3ebf582
Shiv <shivanshityagi@icloud.com>
2022-10-12 15:19:51 +0900

    update

--- components/dao-dashboard/home/sidebar/Entity.tsx
+++ components/dao-dashboard/home/sidebar/Entity.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
-import { Avatar, Button, Card, Heading, Stack, IconArrowRight } from '@kalidao/reality'
+import Image from 'next/image'
+import { Avatar, Text, Button, Card, Heading, Stack, IconArrowRight } from '@kalidao/reality'
 import { useGetWrappr } from '@graph/queries/getWrappr'
 import { useQuery } from '@tanstack/react-query'
 
@@ -9,7 +10,7 @@ type Props = {
 }
 
 const Entity = ({ address, chainId }: Props) => {
-  const { data, error, isLoading } = useGetWrappr(Number(chainId), address)
+  const { data, error, isSuccess, isLoading } = useGetWrappr(Number(chainId), address)
   const tokenURI = data?.uri ? data?.uri : data?.wrappr?.baseURI
   const {
     data: uri,
@@ -30,19 +31,26 @@ const Entity = ({ address, chainId }: Props) => {
   console.log('wrappr', error, isLoading, data)
   console.log('uri', tokenURI, uriError, isLoadingURI, uri)
 
+  const wrapprLink =
+    isSuccess && data
+      ? 'https://www.wrappr.wtf/${chainId}/${data?.wrappr?.id}/${data?.collectionId}'
+      : 'https://www.wrappr.wtf/'
+
   return (
     <Card padding="8" width="112" borderRadius={'large'} shadow hover>
-      <Stack direction={'horizontal'} align="flex-end" justify={'flex-end'}>
+      <Stack direction={'horizontal'} align={'flex-end'} justify={'space-between'}>
         <Stack align="flex-start" justify={'flex-start'} as="a">
-          <Heading>{uri ? uri?.name : data?.wrappr?.name}</Heading>
-          <Avatar as="img" size="40" shape="square" label={'Wrappr Logo'} src={uri ? uri.image : ''} />
+          <Heading>{uri ? uri?.name : data ? data?.wrappr.name : 'Wrapprs'}</Heading>
+          <Text>{!data && 'Add a legal wrappr to your DAO.'}</Text>
+          {/* {!data && <Image src={'/img/wrappr.svg'} height='180' width='180' />} */}
+          {<Avatar as="img" size="40" shape="square" label={'Wrappr Logo'} src={uri ? uri.image : '/img/wrappr.svg'} />}
         </Stack>
         <Button
           as="a"
           shape="circle"
           variant="secondary"
           tone="blue"
-          href={`https://www.wrappr.wtf/${chainId}/${data?.wrappr?.id}/${data?.collectionId}`}
+          href={wrapprLink}
           target="_blank"
           rel="nooppener noreferrer"
         >
