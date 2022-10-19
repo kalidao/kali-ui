21e6e6b92de49b1a2ac59e4c0edd9f819df9af73
audsssy <stamfordh@hotmail.com>
2022-10-19 14:03:14 -0400

    Update SendEth.tsx

--- components/dao-dashboard/newproposal/send/SendEth.tsx
+++ components/dao-dashboard/newproposal/send/SendEth.tsx
@@ -1,31 +1,44 @@
 import React, { useState } from 'react'
 import { ethers } from 'ethers'
-import { useContract, useContractRead, useSigner } from 'wagmi'
-import { Flex, Text, Button } from '../../../../styles/elements'
-import { Form, FormElement, Label, Input } from '../../../../styles/form-elements'
-import FileUploader from '../../../tools/FileUpload'
+import { useContract, useContractRead, useContractWrite, useSigner } from 'wagmi'
+import { FieldSet, Text, Input, Button, Stack } from '@kalidao/reality'
 import KALIDAO_ABI from '../../../../abi/KaliDAO.json'
 import { useRouter } from 'next/router'
-import { uploadIpfs } from '../../../tools/ipfsHelpers'
 import Back from '../../../../styles/proposal/Back'
 import { createProposal } from '../utils/'
 import { ProposalProps } from '../utils/types'
+import ChainGuard from '@components/dao-dashboard/ChainGuard'
+import { AddressZero } from '@ethersproject/constants'
+
 
 export default function SendEth({ setProposal, title, content }: ProposalProps) {
   const router = useRouter()
-  const { dao: daoAddress, chainId: daoChainId } = router.query
+  const { dao, chainId } = router.query
   const { data: daoName, isLoading } = useContractRead({
-    addressOrName: daoAddress ? (daoAddress as string) : ethers.constants.AddressZero,
+    addressOrName: dao ? (dao as string) : AddressZero,
     contractInterface: KALIDAO_ABI,
     functionName: 'name',
-    chainId: Number(daoChainId),
+    chainId: Number(chainId),
   })
   const { data: signer } = useSigner()
 
   const kalidao = useContract({
-    addressOrName: daoAddress ? (daoAddress as string) : ethers.constants.AddressZero,
+    addressOrName: dao ? (dao as string) : AddressZero,
     contractInterface: KALIDAO_ABI,
     signerOrProvider: signer,
+  })
+
+  const {
+    isSuccess: isProposeSuccess,
+    isError: isProposeError,
+    error: proposeError,
+    isLoading: isProposePending,
+    write: propose,
+  } = useContractWrite({
+    mode: 'recklesslyUnprepared',
+    addressOrName: dao as string,
+    contractInterface: KALIDAO_ABI,
+    functionName: 'propose',
   })
 
   // form
@@ -35,11 +48,11 @@ export default function SendEth({ setProposal, title, content }: ProposalProps) 
   // TODO: Popup to change network if on different network from DAO
   const submit = async () => {
     if (!amount) return
-    let amt = amount && ethers.utils.parseEther(amount).toString()
+    let amt = amount && ethers.utils.parseEther(amount.toString())
 
     let docs
     try {
-      docs = await createProposal(daoAddress as string, Number(daoChainId), 2, title, content)
+      docs = await createProposal(dao as string, Number(chainId), 2, title, content)
     } catch (e) {
       console.error(e)
       return
@@ -62,34 +75,52 @@ export default function SendEth({ setProposal, title, content }: ProposalProps) 
   }
 
   return (
-    <Flex dir="col" gap="md">
-      <Text
-        css={{
-          fontFamily: 'Regular',
-        }}
+    <Stack>
+      <FieldSet
+        legend="Send Ether"
+        description={`Send Ether from ${daoName} treasury`}
       >
-        Send ETH from {daoName} treasury
-      </Text>
-      <Form>
-        <FormElement>
-          <Label htmlFor="recipient">Recipient</Label>
-          <Input
-            name="recipient"
-            type="text"
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setRecipient(e.target.value)}
-          />
-        </FormElement>
-        <FormElement>
-          <Label htmlFor="amount">Amount</Label>
-          <Input
-            name="amount"
-            type="number"
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setAmount(e.target.value)}
-          />
-        </FormElement>
-        <Back onClick={() => setProposal?.('sendMenu')} />
-        <Button onClick={submit}>Submit</Button>
-      </Form>
-    </Flex>
+        <Input
+          label="Recipient"
+          // description="The recipient address that will receive Ether"
+          name="recipient"
+          type="text"
+          inputMode="text"
+          placeholder={AddressZero}
+          value={recipient}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setRecipient(e.target.value)}
+        />
+        <Input
+          label="Amount"
+          // description="The amount of Ether to send."
+          name="amount"
+          type="number"
+          inputMode="decimal"
+          placeholder="0"
+          min={0}
+          value={amount}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setAmount(Number(e.target.value))}
+        />
+      </FieldSet>
+      <Stack direction={'horizontal'} justify="space-between">
+        <Back onClick={() => setProposal?.('membersMenu')} />
+        <ChainGuard>
+          <Button
+            center
+            variant="primary"
+            onClick={submit}
+            loading={isProposePending}
+            disabled={!propose || isProposePending || isProposeSuccess}
+          >
+            {isProposePending ? 'Submitting...' : 'Submit'}
+          </Button>
+          <Text>
+            {isProposeSuccess
+              ? 'Proposal submitted on chain!'
+              : isProposeError && `Error submitting proposal: ${proposeError}`}
+          </Text>
+        </ChainGuard>
+      </Stack>
+    </Stack>
   )
 }
