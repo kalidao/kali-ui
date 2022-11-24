d4b07f0345599e7323f93e8f492b17ea3e908afa
shiv <shivanshityagi@icloud.com>
2022-11-24 11:28:28 +0900

    Update privacy.tsx

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -12,6 +12,7 @@ export default function Privacy() {
       <Box paddingX={'10'} paddingY="2">
         <ReactMarkdown
           components={{
+            h1: ({ node, ...props }) => <h2 style={{ color: 'white' }} {...props} />,
             h2: ({ node, ...props }) => <h2 style={{ color: 'white' }} {...props} />,
             p: ({ node, ...props }) => <p style={{ color: '#F9F6EE' }} {...props} />,
             li: ({ node, ...props }) => <li style={{ color: '#F9F6EE ' }} {...props} />,
