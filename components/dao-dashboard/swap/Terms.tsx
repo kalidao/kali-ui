7deace5915c5134453f4fcdfadee564f232005ae
Shiv <shivanshityagi@icloud.com>
2022-12-08 17:36:21 +0530

    🥢 terms swap

--- components/dao-dashboard/swap/Terms.tsx
+++ components/dao-dashboard/swap/Terms.tsx
@@ -1,14 +1,11 @@
+import Link from 'next/link'
 import { Box, Checkbox } from '@kalidao/reality'
 import { useSwapStore } from './store'
 
 export default function Terms() {
-  const details = useSwapStore((state) => state.swap.details)
-
   const setConsent = useSwapStore((state) => state.setConsent)
-  const consent = useSwapStore((state) => state.consent)
-
-  console.log('details', details)
-  return <Checkbox size="small" label={<Box>
-    I agree to the <a href={details} target="_blank" rel="noreferrer">terms and conditions</a>
+ 
+  return <Checkbox size="small" label={<Box color="text">
+    I agree to the <a href={'/tos'} target="_blank" rel="noreferrer">terms and conditions</a>.
   </Box>} onCheckedChange={setConsent}></Checkbox>
 }
