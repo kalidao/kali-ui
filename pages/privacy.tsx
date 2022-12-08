bda08638579d2e7304ae8d480b1bb726b66af102
Shiv <shivanshityagi@icloud.com>
2022-12-08 16:10:23 +0530

    🥢 tos styling

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -2,22 +2,23 @@ import ReactMarkdown from 'react-markdown'
 import Layout from '../components/layout'
 import { Box } from '@kalidao/reality'
 import { privacy } from '../constants/privacy'
+import { useThemeStore } from '@components/hooks/useThemeStore'
 
 export default function Privacy() {
+  const mode = useThemeStore((state) => state.mode)
   return (
     <Layout
       heading="KaliCo Privacy Policy"
       content="The terms contained here define our relationship with each other."
     >
-      <Box paddingX={'10'} paddingY="2">
+      <Box padding="6" backgroundColor="backgroundSecondary">
         <ReactMarkdown
           components={{
-            h1: ({ node, ...props }) => <h2 style={{ color: 'white' }} {...props} />,
-            h2: ({ node, ...props }) => <h2 style={{ color: 'white' }} {...props} />,
-            p: ({ node, ...props }) => <p style={{ color: '#F9F6EE' }} {...props} />,
-            li: ({ node, ...props }) => <li style={{ color: '#F9F6EE ' }} {...props} />,
-            // Rewrite `em`s (`*like so*`) to `i` with a red foreground color.
-            em: ({ node, ...props }) => <i style={{ color: 'blue' }} {...props} />,
+            h1: ({ node, ...props }) => <h2 style={{ color: mode === 'dark' ? 'white' : 'black' }} {...props} />,
+            h2: ({ node, ...props }) => <h2 style={{ color: mode === 'dark' ? 'white' : 'black' }} {...props} />,
+            p: ({ node, ...props }) => <p style={{ color: mode === 'dark' ? 'white' : 'black' }} {...props} />,
+            li: ({ node, ...props }) => <li style={{ color: mode === 'dark' ? 'white' : 'black' }} {...props} />,
+            em: ({ node, ...props }) => <i style={{ color: mode === 'dark' ? 'white' : 'black' }} {...props} />,
           }}
         >
           {privacy}
--- pages/tos.tsx
+++ pages/tos.tsx
@@ -2,8 +2,10 @@ import ReactMarkdown from 'react-markdown'
 import Layout from '../components/layout'
 import { Box } from '@kalidao/reality'
 import { tos } from '../constants/tos'
+import { useThemeStore } from '@components/hooks/useThemeStore'
 
 export default function ToS() {
+  const mode = useThemeStore((state) => state.mode)
   return (
     <Layout
       heading="KaliCo Terms of Service"
@@ -11,13 +13,13 @@ export default function ToS() {
     >
       <Box padding="6" backgroundColor="backgroundSecondary">
         <ReactMarkdown
-          components={{
-            h2: ({ node, ...props }) => <h2 style={{ color: 'white' }} {...props} />,
-            p: ({ node, ...props }) => <p style={{ color: '#F9F6EE' }} {...props} />,
-            li: ({ node, ...props }) => <li style={{ color: '#F9F6EE' }} {...props} />,
-            // Rewrite `em`s (`*like so*`) to `i` with a red foreground color.
-            em: ({ node, ...props }) => <i style={{ color: '#F9F6EE' }} {...props} />,
-          }}
+            components={{
+              h1: ({ node, ...props }) => <h2 style={{ color: mode === 'dark' ? 'white' : 'black' }} {...props} />,
+              h2: ({ node, ...props }) => <h2 style={{ color: mode === 'dark' ? 'white' : 'black' }} {...props} />,
+              p: ({ node, ...props }) => <p style={{ color: mode === 'dark' ? 'white' : 'black' }} {...props} />,
+              li: ({ node, ...props }) => <li style={{ color: mode === 'dark' ? 'white' : 'black' }} {...props} />,
+              em: ({ node, ...props }) => <i style={{ color: mode === 'dark' ? 'white' : 'black' }} {...props} />,
+            }}
         >
           {tos}
         </ReactMarkdown>
