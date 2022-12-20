5efc266553f7b64ce4ef5749ff9968e8c14bba8e
audsssy <stamfordh@hotmail.com>
2022-12-20 17:07:57 -0500

    update contract

--- constants/addresses.ts
+++ constants/addresses.ts
@@ -102,7 +102,7 @@ export const addresses: { [key: number]: any } =
         crowdsale2: '0xB682e773768e68C02B8b3892CF32eA090600b4b4',
         // crowdsale2: '0x2350C968C7B323Ad255E3942fcb9d578638792EC',
         redemption: '0x2b8f116e4D9E73A3A9E7CAF1655B9FC01588Db8d',
-        manager: '0x01100BcA3ca6265F367Bf028C224DA5200eFE0d7',
+        project: '0x9BDE417544bc2A281150972A58C9a56F59cbaFa5',
       },
       blockExplorer: 'https://goerli.etherscan.io/',
     },
