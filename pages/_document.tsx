daf507de5c2997fc907edf8eeb6cb0bd4ed84479
Shiv <shivanshityagi@icloud.com>
2022-12-24 17:32:49 +0530

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,30 +1,36 @@
 import React from 'react'
 import Document, { DocumentContext, DocumentInitialProps, Html, Head, Main, NextScript } from 'next/document'
 
-interface MyDocumentProps extends DocumentInitialProps {
+interface Props {
   theme: string
 }
 
-export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+export default class MyDocument extends Document<Props> {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps & Props> {
     const initialProps = await Document.getInitialProps(ctx)
 
     let theme = 'dark'
-    if (ctx.req && ctx.req.headers.cookie) {
-      console.log('cookie values: ', ctx.req.headers.cookie)
-      const cookies = ctx.req.headers.cookie.split(';')
-      const mode = cookies.find((cookie) => cookie.includes('mode'))?.split('=')[1]
-      console.log('mode: ', mode)
-      theme = mode === 'dark' ? 'dark' : 'light'
+    const cookies = ctx?.req?.headers?.cookie
+    if (cookies) {
+      const themeCookie = cookies.split(';').find((c) => c.trim().startsWith('mode='))
+      if (themeCookie) {
+        theme = themeCookie.split('=')[1]
+      }
+      console.log('cookies: ', cookies)
     }
 
-    return { ...initialProps, theme } as MyDocumentProps
+    return { ...initialProps, theme }
   }
 
   render() {
-    console.log('theme: ', this.props)
+    console.log('theme: ', this.props.theme)
     return (
-      <Html lang="en">
+      <Html
+        lang="en"
+        style={{
+          backgroundColor: this.props.theme === 'dark' ? '#000' : '#fff',
+        }}
+      >
         <Head>
           <link
             rel="preload"
